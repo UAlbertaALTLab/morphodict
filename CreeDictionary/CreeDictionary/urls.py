c415b73734f28a06d5c994e2bd6ce02339358069
Cosh_ <cosh@slouple.com>
2019-02-18 21:08:47 -0700

    Init Project Structure

---
+++ CreeDictionary/CreeDictionary/urls.py
@@ -0,0 +1,21 @@
+"""
+Definition of urls for CreeDictionary.
+"""
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', CreeDictionary.views.home, name='home'),
+    # url(r'^CreeDictionary/', include('CreeDictionary.CreeDictionary.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
