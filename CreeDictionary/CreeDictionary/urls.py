3e262bffd920f56cee86c833da9772ac5f3a5e4b
99sshimizu <sshimizu@ualberta.ca>
2019-02-25 10:58:59 -0700

    Based on Tutorial

--- CreeDictionary/CreeDictionary/urls.py
+++ CreeDictionary/CreeDictionary/urls.py
@@ -5,18 +5,18 @@ Definition of urls for CreeDictionary.
 from django.conf.urls import include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
-    path('', include('React.urls')),
+               #path('', include('React.urls')),
     # Examples:
     # url(r'^$', CreeDictionary.views.home, name='home'),
     # url(r'^CreeDictionary/', include('CreeDictionary.CreeDictionary.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 ]
--- CreeDictionary/React/urls.py
+++ CreeDictionary/React/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path
+#from django.urls import path
+from django.conf.urls import include, url
 from . import views
 urlpatterns = [
-    path('', views.index ),
+    url(r'^$', views.index, name='home')
+               #path('', views.index ),
 ]
