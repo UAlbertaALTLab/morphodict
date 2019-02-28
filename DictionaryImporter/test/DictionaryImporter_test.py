2be133d5b0fe868ff8989992530e1d98bbe202d6
Cosh_ <cosh@slouple.com>
2019-02-28 10:05:42 -0700

    Added Importer and Parser Tests

---
+++ DictionaryImporter/test/DictionaryImporter_test.py
@@ -0,0 +1,31 @@
+﻿import unittest
+from unittest import mock
+from DictionaryImporter import *
+import json
+
+"""
+Tests should be run at the root folder of DictionaryImporter project
+"""
+class DictionaryImporrterTestCase(unittest.TestCase):
+    def setUp(self):
+        self.importer = DictionaryImporter("../CreeDictionary/API/dictionaries/crkeng.xml", "../CreeDictionary/db.sqlite3", 
+                                  "../CreeDictionary/API/dictionaries/crk-analyzer.fomabin.gz", "../CreeDictionary/API/dictionaries/crk-generator.fomabin.gz", 
+                                  "../CreeDictionary/API/paradigm/", "crk")
+        self.importer.processCount = 0
+
+    def tearDown(self):
+        self.importer = None
+
+    def test_get_entry_id(self):
+        #Mock FST.from_file() to speed up test
+        with mock.patch('DictionaryImporter.FST.from_file') as mockFST:
+            mockFST.return_value = None
+            self.importer.processCount = 8
+            self.importer._loadParadigmFiles()
+            self.importer._initProcessFields(3, None, None, None, None, None, None)
+            self.assertIs(self.importer._getEntryID(Word), 3)
+            self.assertIs(self.importer._getEntryID(Word), 11)
+            self.assertIs(self.importer._getEntryID(Word), 19)
+            self.assertIs(self.importer._getEntryID(Lemma), 3)
+            self.assertIs(self.importer._getEntryID(Lemma), 11)
+            self.assertIs(self.importer._getEntryID(Inflection), 3)
