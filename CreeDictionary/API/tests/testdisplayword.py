5dd80c1b3e8fa2fe24108563500cb0fa905d262b
Cosh_ <cosh@slouple.com>
2019-03-21 16:56:23 -0600

    Added Django API Tests

--- CreeDictionary/API/tests/testdisplayword.py
+++ CreeDictionary/API/tests/testdisplayword.py
@@ -1,10 +1,18 @@
 import django
 from django.test import TestCase, Client
 from API.views import displayWord
+from API.models import *
 from urllib.parse import quote
+import json
 
 class DisplayWordTest(TestCase):
-
+    @classmethod
+    def setUpTestData(cls):
+        Lemma.objects.create(id=1, context="mitâs", language="crk", type="N")
+        Definition.objects.create(id=1, fk_word_id=1, source="CW", context="Pants.")
+        Inflection.objects.create(id=2, fk_lemma_id=1, context="mitâsa", type="N", language="crk")
+        InflectionForm.objects.create(id=1, fk_inflection_id=2, name="N")
+        InflectionForm.objects.create(id=2, fk_inflection_id=2, name="A")
     @classmethod
     def setUpClass(cls):
         super(DisplayWordTest, cls).setUpClass()
@@ -18,3 +26,11 @@ class DisplayWordTest(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
+    def test_inflection(self):
+        queryString = quote("mitâs")
+        response = self.client.get("/DisplayWord/" + queryString)
+        self.assertEqual(response.status_code, 200)
+        content = json.loads(response.content)
+        self.assertTrue("lemma" in content)
+        self.assertEqual(len(content["inflections"]), 1)
+        self.assertEqual(len(content["inflections"][0]["inflectionForms"]), 2)
