db948b87d33cf81b8e3cfdcfa8328e3420326e7f
99sshimizu <sshimizu@ualberta.ca>
2019-04-04 00:46:00 -0600

    added fixture json

--- CreeDictionary/React/cypressTest/cypress/integration/app_spec.js
+++ CreeDictionary/React/cypressTest/cypress/integration/app_spec.js
@@ -2,27 +2,38 @@
 describe("Django REST framework / cree intelligent dictionary app", () => {
    // Currently using...
    //       word: mitâs 
+   //       width of screen: 1400pixel
+   //       height of screen: 900pixel
    // for testing
    const state = {
       word: "mitâs",
       wordLanguage: "Cree",
       wordCategory: "Noun",
+      height: 900,
+      width: 1400,
    };
    //Connect to the web
    before(() => {
       cy.exec("npm run dev");
+      cy.viewport(state.width, state.height)
    });
+   beforeEach(() => {
+      cy.fixture("cree/creeWordSearch").as("cree");
+    });
    //Test for Search form
    it("Search Area show up", () => {
       cy.visit("/");
-      cy.get("form");
+      cy.get(".sidebar").should("be.visible");
+      cy.get("form").should("be.visible");
+      cy.get("input").should("have.value", "Enter Word");
+      cy.get("input").type(this.cree.word).should("have.value", this.cree.word);
    });
    //Test for return when serch word
    it("List the search result", () => {
-      cy.get("input").type(state.word);
-      cy.get("#Search").submit();
+      //cy.get("input").type(this.cree.word);
+      //cy.get("#Search").submit();
       //cy.wait(5000)
-      //cy.get("li").should("contain", state.word);
+      //cy.get(".card-title");
    });
    //Test for detail of selected word
    it("Onclick on word and gains detail", () => {
