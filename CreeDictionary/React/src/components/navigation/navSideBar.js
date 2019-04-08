e63da2403cefb3b367908e66bdc096aee1e2bb5c
Cosh_ <cosh@slouple.com>
2019-04-08 06:43:41 -0600

    Changed color

--- CreeDictionary/React/src/components/navigation/navSideBar.js
+++ CreeDictionary/React/src/components/navigation/navSideBar.js
@@ -43,16 +43,16 @@ class NavSideBar extends React.Component {
                 <div className="sidebar-wrapper">
                     <ul className="nav">
                         <li>
-                            <a href="/"><h4 className="text-default">{i18next.t('Home')}</h4></a>
+                            <a href="/"><h4>{i18next.t('Home')}</h4></a>
                         </li>
                         <li>
-                            <a onClick={() => this.translate("cree")}><h4 className="text-default">Cree</h4></a>
+                            <a onClick={() => this.translate("eng")}><h4>English</h4></a>
                         </li>
                         <li>
-                            <a onClick={() => this.translate("eng")}><h4 className="text-default">English</h4></a>
+                            <a onClick={() => this.translate("cree")}><h4>Cree</h4></a>
                         </li>
                         <li>
-                            <a onClick={() => this.translate("syllabic")}><h4 className="text-default">Syllabic</h4></a>
+                            <a onClick={() => this.translate("syllabic")}><h4>Syllabic</h4></a>
                         </li>
                     </ul>
                 </div>
