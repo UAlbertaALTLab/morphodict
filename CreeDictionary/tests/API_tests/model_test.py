ba2ff7f302e2f56b02fd61775fc20780361575c4
matt <syan4@ualberta.ca>
2019-08-29 16:10:29 -0600

    started API model methods to fetch data

--- CreeDictionary/tests/API_tests/model_test.py
+++ CreeDictionary/tests/API_tests/model_test.py
@@ -1,31 +1,48 @@
-# from typing import Iterable
-#
-# import pytest
-#
-# from API.models import Inflection
-#
-#
-# @pytest.mark.django_db
-# @pytest.mark.parametrize(
-#     ("corpus", "query", "distance", "expected"),
-#     (
-#         [("a", "b", "c"), "a", 1, (0, 1, 2)],
-#         [("a", "bb", "c"), "a", 1, (0, 2)],
-#         [("a", "ab", "c"), "a", 1, (0, 1, 2)],
-#         [("a", "ab", "c"), "ab", 1, (0, 1)],
-#         [("banana", "bapapa", "papapa"), "banana", 2, (0, 1)],
-#     ),
-# )
-# def test_search(
-#     corpus: Iterable[str], query: str, distance: int, expected: Iterable[int]
-# ):
-#     for i, word in enumerate(corpus):
-#         Inflection(id=i, text=word, analysis="", is_lemma=True, as_is=True).save()
-#
-#     fuzzy_searcher = CreeFuzzySearcher()
-#     assert set(fuzzy_searcher.search(query, distance)) == set(
-#         Inflection.objects.filter(id__in=expected)
-#     )
-#     # CreeFuzzySearcher is a singleton class. Destroy the created instance
-#     assert CreeFuzzySearcher() is fuzzy_searcher
-#     CreeFuzzySearcher._instance = None
+import random
+
+from hypothesis import given
+from hypothesis._strategies import composite, integers
+from hypothesis.extra.django import from_model
+import pytest
+from API.models import Inflection
+
+
+@pytest.fixture(scope="module")
+def fff(request):
+    print("123", vars(request.module))
+    for i in range(random.randint(1, 20)):
+        Inflection.objects.create(id=i).save()
+    return 1
+
+
+@composite
+def shoot_inflections(draw):
+    i = draw(integers())
+    print("from shoot strategy:", Inflection.objects.all().count())
+    return i
+
+
+# @given(inflection=shoot_inflections())
+def test_stuff(crk_eng_hundredth_file):
+    a = 1
+    pass
+    # print("jj", lmaomlao)
+    # print("yy", Inflection.objects.all().count())
+    # print(inflection)
+    assert 1 == 1
+
+
+# @given(inflection=shoot_inflections())
+# def test_new_stuff(stuff, inflection):
+#     a = 1
+#     print(inflection)
+#     assert 1 == 1
+
+
+@composite
+def analyzable_db_lemmas(draw) -> Inflection:
+    """
+    lemmas with as_is being false
+    """
+    print(vars().items())
+    pass
--- CreeDictionary/tests/conftest.py
+++ CreeDictionary/tests/conftest.py
@@ -1,10 +1,35 @@
-# todo: database fixture
-# https://pytest-django.readthedocs.io/en/latest/database.html#populate-the-database-with-initial-test-data
-# import pytest
-#
-# from django.core.management import call_command
-#
-# @pytest.fixture(scope='session')
-# def django_db_setup(django_db_setup, django_db_blocker):
-#     with django_db_blocker.unblock():
-#         call_command('loaddata', 'your_data_fixture.json')
+from os.path import dirname
+from pathlib import Path
+
+import pytest
+import xml_subsetter
+
+from utils import shared_res_dir
+
+
+@pytest.fixture
+def topmost_datadir():
+    return Path(dirname(__file__)) / "data"
+
+
+@pytest.fixture
+def crk_eng_hundredth_file(topmost_datadir) -> Path:
+    """
+    1/100 of the entries in the real crkeng.xml
+    """
+
+    hundredths_file = Path(topmost_datadir) / "crkeng_hundredth.xml"
+
+    def create_crkeng_hundredth():
+        """
+        create the file if it does not already exist
+        """
+        crkeng_file = shared_res_dir / "dictionaries" / "crkeng.xml"
+        if not crkeng_file.exists():
+            raise FileNotFoundError("%s not found" % crkeng_file)
+        xml_subsetter.subset_head(crkeng_file, hundredths_file, "e", 0.01)
+
+    if not hundredths_file.exists():
+        create_crkeng_hundredth()
+
+    return hundredths_file
