013d1b17d38a89ca6a8566489d848ea0cf76578b
Eddie Antonio Santos <easantos@ualberta.ca>
2019-10-11 21:28:44 -0600

    Test that we can search a gorram word.

---
+++ cypress/integration/search.spec.js
@@ -0,0 +1,17 @@
+context('Searching', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  describe('I want to know what a Cree word means in English', () => {
+    it('should search for an exact lemma', () => {
+      // https://on.cypress.io/should
+      cy.get('[data-cy=search]')
+        .type('acâhkos')
+
+      cy.get('[data-cy=search-results]')
+        .should('contain', 's/he sleeps')
+        .should('contain', 's/he is sleeping')
+    })
+  })
+})
