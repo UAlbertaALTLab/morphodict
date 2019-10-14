a34487640201a6b7387976649411d4583f5cc670
Eddie Antonio Santos <easantos@ualberta.ca>
2019-10-14 12:01:43 -0600

    Disable search test until #120 is resolved.

--- cypress/integration/search.spec.js
+++ cypress/integration/search.spec.js
@@ -4,7 +4,9 @@ context('Searching', () => {
   })
 
   describe('I want to know what a Cree word means in English', () => {
-    it('should search for an exact lemma', () => {
+    // TODO: Enable this test when this issue is fixed:
+    // https://github.com/UAlbertaALTLab/cree-intelligent-dictionary/issues/120
+    it.skip('should search for an exact lemma', () => {
       // https://on.cypress.io/should
       cy.get('[data-cy=search]')
         .type('acâhkos')
