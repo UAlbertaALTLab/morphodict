e1b18ad547b0e4a994a1232bc37714f90ea0809d
Eddie Antonio Santos <easantos@ualberta.ca>
2019-10-14 19:02:34 -0600

    Test searching by going directly to the URL.

--- cypress/integration/search.spec.js
+++ cypress/integration/search.spec.js
@@ -1,18 +1,21 @@
 context('Searching', () => {
-  beforeEach(() => {
-    cy.visit('/')
-  })
-
   describe('I want to know what a Cree word means in English', () => {
     // TODO: Enable this test when this issue is fixed:
     // https://github.com/UAlbertaALTLab/cree-intelligent-dictionary/issues/120
     it.skip('should search for an exact lemma', () => {
-      // https://on.cypress.io/should
+      cy.visit('/')
       cy.get('[data-cy=search]')
         .type('acâhkos')
 
       cy.get('[data-cy=search-results]')
         .should('contain', 'star')
+    })
+
+    it('should perform the search by going directly to the URL', () => {
+      cy.visit('/search/amisk')
+
+      cy.get('[data-cy=search-results]')
+        .should('contain', 'beaver')
     })
   })
 })
