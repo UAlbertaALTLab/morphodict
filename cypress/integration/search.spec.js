7c7f80b603dae8c4a97c297cff5c207028a63c97
Eddie Antonio Santos <easantos@ualberta.ca>
2019-10-14 19:24:15 -0600

    Test for a loading indicator.

--- cypress/integration/search.spec.js
+++ cypress/integration/search.spec.js
@@ -17,5 +17,41 @@ context('Searching', () => {
       cy.get('[data-cy=search-results]')
         .should('contain', 'beaver')
     })
+
+  })
+  describe('Loading indicator', () => {
+    beforeEach(() => {
+      cy.server()
+      cy.visit('/')
+    })
+
+    it('should display a loading indicator', () => {
+      cy.route({
+        url: '/_search_results/**',
+        delay: 100, // milliseconds of delay
+        response: `<ol><li data-cy="search-results">
+            <span lang="cr">amisk</span>: beaver
+        </li></ol>`
+      }).as('search')
+
+      cy.get('[data-cy=search]')
+        .as('searchBox')
+
+      // Initially, there should be no search results visible:
+      cy.get('body')
+        .not('[data-cy=search-results]')
+      // We type our search, like normal:
+      cy.get('@searchBox')
+        .type('amisk')
+
+      // This the loading indicator should be visible!
+      cy.get('[data-loading-indicator]')
+        .should('be.visible')
+
+      // Wait for the results to come back
+      cy.wait('@search')
+      cy.get('[data-loading-indicator]')
+        .should('not.be.visible')
+    })
   })
 })
