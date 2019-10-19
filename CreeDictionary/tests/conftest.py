4b0bc6ca8d9595b4993071b13490bfebc4a233d9
Eddie Antonio Santos <easantos@ualberta.ca>
2019-10-18 21:31:37 -0600

    Write a better query for random_lemmas() strategy.

--- CreeDictionary/tests/conftest.py
+++ CreeDictionary/tests/conftest.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import pytest
 import xml_subsetter
 from django.db import transaction
+from django.db.models import F
 from hypothesis import assume
 from hypothesis._strategies import composite, integers, sampled_from
 
@@ -46,15 +47,16 @@ def random_lemmas(draw) -> Inflection:
     """
     Strategy that supplies wordforms that are also lemmas!
     """
-    lemmas = list(Inflection.objects.filter(is_lemma=True, as_is=False))
     # Sometimes two different wordforms are marked as lemmas of the same word,
     # and yet they have different spelling. (Ed: that's not... what the word
     # "lemma" means, but okay...). So make sure to get the DEFAULT spelling
     # only!
     # e.g., akocin and akociniw are both two different spellings of the same
     # lemma.
-    lemmas = [lemma for lemma in lemmas if lemma.default_spelling.id == lemma.id]
-    return draw(sampled_from(lemmas))
+    lemmas = Inflection.objects.filter(
+        is_lemma=True, as_is=False, pk=F("default_spelling__pk")
+    )
+    return draw(sampled_from(list(lemmas)))
 
 
 @composite
