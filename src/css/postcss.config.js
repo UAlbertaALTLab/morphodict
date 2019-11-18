24cb4178e7fe464b8e0b8e4bdb9295b4bcea4f2c
Eddie Antonio Santos <easantos@ualberta.ca>
2019-11-18 10:52:28 -0700

    Use PostCSS to generate styles.css.

---
+++ src/css/postcss.config.js
@@ -0,0 +1,33 @@
+/**
+ * PostCSS config.
+ */
+
+const postcssCustomProperties = require('postcss-custom-properties')
+const postcssImport = require('postcss-import')
+
+module.exports = {
+  plugins: [
+    /**
+     * Inline @import rules (e.g., @import "normalize.css").
+     */
+    postcssImport(),
+    /**
+     * Turns CSS Custom Properties (a.k.a., variables) into old school
+     * declarations. e.g.,
+     *   :root {
+     *    --my-var: blue;
+     *   }
+     *   p {
+     *    color: var(--my-var);
+     *   }
+     *
+     * becomes
+     *
+     *  p {
+     *    color: blue;
+     *    color: var(--my-var);
+     *  }
+     */
+    postcssCustomProperties(),
+  ]
+}
