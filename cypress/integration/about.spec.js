a169a289c48f42b2348c3816586e1836006513e5
Eddie Antonio Santos <easantos@ualberta.ca>
2019-11-19 12:58:56 -0700

    Test for the existance of a few headers.

--- cypress/integration/about.spec.js
+++ cypress/integration/about.spec.js
@@ -2,14 +2,26 @@
  * General tests about the website's behaviour as a regular website.
  */
 context('The About page', function () {
+  describe('Visiting any page', () => {
+    it('should have a link to the about page in the footer', () => {
+      cy.visit('/')
+      cy.get('footer')
+        .contains('a', 'About')
+    })
+
+  })
+
   beforeEach(function () {
     cy.visit('/about')
   })
 
   describe('Visiting the about page', () => {
-    it('should have a link in the footer', () => {
-      cy.get('footer')
-        .contains('a', 'About')
+    it('should have a few required sections', () => {
+      cy.get('main').contains('h2', /Source Materials/i)
+      cy.get('main').contains('h2', 'Credits')
+      cy.get('main').contains('h2', /Contact Us/i)
     })
+
+    it('should have sponsers logos')
   })
 })
