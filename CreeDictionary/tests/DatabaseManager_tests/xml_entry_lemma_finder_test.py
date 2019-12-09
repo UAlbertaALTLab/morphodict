5b8d4f206761bada714d536ec493d7c66a93f887
Madoshakalaka <44753941+Madoshakalaka@users.noreply.github.com>
2019-12-09 10:34:20 -0700

    new test for lemma resolution

--- CreeDictionary/tests/DatabaseManager_tests/xml_entry_lemma_finder_test.py
+++ CreeDictionary/tests/DatabaseManager_tests/xml_entry_lemma_finder_test.py
@@ -50,12 +50,28 @@ def test_lemma_extraction_no_analysis_entry(
 
 # regarding the cause of ambiguity and its consequences. See:
 # https://github.com/UAlbertaALTLab/cree-intelligent-dictionary/issues/176
+# explanation of the resolution process of these two test cases:
+
+
+# askiy (label in xml file: N NI-2)
+# askiy   askiya+N+I+Sg
+# askiy   askiy+N+I+Sg
+
+# ayiwinis (label in xml file: N NI-2)
+# ayiwinis        ayiwinisa+N+I+Sg
+# ayiwinis        ayiwin+N+I+Der/Dim+N+I+Sg
+
+# askiy has two potential NI analyses. The second wordform look the same, choose the second
+
+# ayiwinis has two potential NI analysis, non look the same, label it as ambiguous
+
+
 @pytest.mark.parametrize(
     ("xml_lemma_to_pos_lc", "expected_xml_lemma_pos_lc_to_fst_lemma"),
     [
         (
             {"askiy": [("N", "NI-2")], "ayiwinis": [("", "NI-1")]},
-            {("askiy", "N", "NI-2"): "", ("ayiwinis", "", "NI-1"): ""},
+            {("askiy", "N", "NI-2"): "askiy+N+I+Sg", ("ayiwinis", "", "NI-1"): ""},
         )
     ],
 )
