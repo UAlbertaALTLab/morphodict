1f9e0a6087cc6dd5fd80b69d329b736e41d3f24f
Eddie Antonio Santos <easantos@ualberta.ca>
2019-12-12 09:49:43 -0700

    Optionally download from itwêwina.

--- CreeDictionary/utils/update_layouts.py
+++ CreeDictionary/utils/update_layouts.py
@@ -5,10 +5,12 @@ Updates layout files. Can download them from the old itwêwina -- not
 recommended.
 """
 
+import argparse
 import subprocess
 import sys
+from contextlib import contextmanager
 from glob import glob
-from os import chdir
+from os import chdir, getcwd
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
@@ -17,17 +19,25 @@ from utils.paradigm_layout_combiner import combine_layout_paradigm
 
 
 def update():
-    update_from_old_itwewina()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--download-from-old-itwewina", default=False, action="store_true"
+    )
+    args = parser.parse_args()
+
+    if args.download_from_old_itwewina:
+        download_from_old_itwewina()
+
+    combine_layout_paradigm()
 
 
-def update_from_old_itwewina():
+def download_from_old_itwewina():
     """
     1. Pull layout AND paradigm files from itwewina and place them correctly
     2. combine them to res/prefilled-layouts/
     https://github.com/UAlbertaALTLab/itwewina/tree/development/neahtta/configs/language_specific_rules/paradigms/crk
     """
-    with TemporaryDirectory() as temp_dir:
-        chdir(temp_dir)
+    with TemporaryDirectory() as temp_dir, cd(temp_dir):
         subprocess.run(
             [
                 "gitdir",
@@ -51,4 +61,13 @@ def update_from_old_itwewina():
             Path(paradigm).rename(
                 str(shared_res_dir / "paradigms" / Path(paradigm).name)
             )
-        combine_layout_paradigm()
+
+
+@contextmanager
+def cd(path):
+    curdir = getcwd()
+    try:
+        chdir(curdir)
+        yield
+    finally:
+        chdir(curdir)
