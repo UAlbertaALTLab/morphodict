13dc89a8090281d5c403a91cbccde95e00d20442
Madoshakalaka <44753941+Madoshakalaka@users.noreply.github.com>
2019-12-22 10:04:43 -0700

    Cree-lev™ reforged with Love

--- CreeDictionary/tests/utils_tests/test_cree_lev_dist.py
+++ CreeDictionary/tests/utils_tests/test_cree_lev_dist.py
@@ -2,18 +2,10 @@ from string import ascii_letters
 
 import pytest
 from Levenshtein import distance
-from Levenshtein import editops
 from hypothesis import given, assume
 from hypothesis.strategies import text
 
 from utils import get_modified_distance
-from utils.cree_lev_dist import EditOp
-
-
-@given(text(alphabet=ascii_letters), text(alphabet=ascii_letters))
-def test_apply_ops(s: str, t: str):
-    ops = editops(s, t)
-    assert EditOp.apply_ops(ops, s, t) == t
 
 
 @given(text(alphabet=ascii_letters), text(alphabet=ascii_letters))
@@ -28,13 +20,13 @@ def test_get_distance_basic_lev(spelling: str, normal_form: str):
     )
 
 
-# todo: use real world spelling examples here
 @pytest.mark.parametrize(
     ("spelling", "normal_form", "expected_distance"),
     (
         ["atâk", "atâhk", 0.5],  # an 'h' in a rime is not added
         ["ah", "a", 0.5],  # an extra 'h' in a rime is added
         ["atahk", "atâhk", 0.5],  # a circumflex is not added
+        ["wâpamew", "wâpamêw", 0],  # a circumflex on 'e' is omitted, don't punish
         ["atak", "atâhk", 1],  # both circumflex and h in a rime is not added
         [
             "adak",
