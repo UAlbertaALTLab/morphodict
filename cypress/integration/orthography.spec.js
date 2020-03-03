4194f71c64ddf970ce4c46c8d0bfcf5cc70df44e
Eddie Antonio Santos <easantos@ualberta.ca>
2020-03-03 09:00:27 -0700

    Test for orthography selector.

---
+++ cypress/integration/orthography.spec.js
@@ -0,0 +1,24 @@
+describe('Orthography selection', function () {
+  describe('switching orthography', function () {
+    it('should switch to syllabics when I click on the menu', function () {
+      cy.visit('/')
+
+      // Get the introduction: it should be in SRO
+      cy.contains('h1', 'tânisi!')
+        .as('greeting')
+
+      // Switch to syllabics
+      cy.get('[data-orthography-selector]')
+        .as('menu')
+        .click()
+      cy.get('@menu')
+        .contains('Syllabics')
+        .should('be.visible')
+        .click()
+
+      // Now it should be in syllabics!
+      cy.get('@greeting')
+        .contains('ᑖᓂᓯ!')
+    })
+  })
+})
