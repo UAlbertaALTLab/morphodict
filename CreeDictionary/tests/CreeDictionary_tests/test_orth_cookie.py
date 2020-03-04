eefb9483a538ee52cc9c18f1a8e64970e6da590b
Eddie Antonio Santos <easantos@ualberta.ca>
2020-03-04 12:16:45 -0700

    Parametrize test with all orthographies.

--- CreeDictionary/tests/CreeDictionary_tests/test_orth_cookie.py
+++ CreeDictionary/tests/CreeDictionary_tests/test_orth_cookie.py
@@ -11,9 +11,11 @@ import pytest
 from django.urls import reverse
 
 
-def test_can_set_orth_cookie(client):
-    orth = "Cans"
-
+@pytest.mark.parametrize("orth", ["Cans", "Latn", "Latn-x-macron"])
+def test_can_set_orth_cookie(orth, client):
+    """
+    Test that POSTing to the URL actually sets the orthography.
+    """
     # By default, there should be no orth cookie.
     index_url = reverse("cree-dictionary-index")
     response = client.get(index_url)
@@ -34,6 +36,10 @@ def test_can_set_orth_cookie(client):
 
 @pytest.mark.parametrize("method", ["get", "head"])
 def test_bad_methods(client, method):
+    """
+    We should not be able to GET or HEAD change-orthography URL.
+    """
+
     change_orth_url = reverse("cree-dictionary-change-orthography")
     make_request = getattr(client, method)
     response = make_request(change_orth_url, {"orth": "Cans"})
