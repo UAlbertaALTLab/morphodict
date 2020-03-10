d9dd694fef159a1af208aae2af11376950bc2847
Eddie Antonio Santos <easantos@ualberta.ca>
2020-03-10 13:43:27 -0600

    Test non-ASCII characters in query string.

--- CreeDictionary/tests/CreeDictionary_tests/test_utils.py
+++ CreeDictionary/tests/CreeDictionary_tests/test_utils.py
@@ -7,7 +7,9 @@ import pytest
 from CreeDictionary.utils import url_for_query
 
 
-@pytest.mark.parametrize("query", ["awa"])
+@pytest.mark.parametrize("query", ["awa", "wâpamêw", "ᐚᐸᒣᐤ"])
 def test_query_always_ascii(query: str):
     url = url_for_query(query)
-    assert all(c in ascii_printable for c in url)
+    assert all(
+        c in ascii_printable for c in url
+    ), f"{url!r} should not contain non-ascii characters"
