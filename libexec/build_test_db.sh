617c4aaec88f78b5c537aca63bcf7b482c165b49
matt <syan4@ualberta.ca>
2020-03-10 17:36:17 -0600

    use shell scripts

--- libexec/build_test_db.sh
+++ libexec/build_test_db.sh
@@ -1,3 +1,12 @@
+# should be called from pipfile
 
 # 0005 was the basis of the import script in Database Manager
-rm CreeDictionary/test_db.sqlite3 && pipenv run python CreeDictionary/manage.py migrate API 0005 && manage-db build-test-db
+# -i makes it interactive, ask user to comfirm before he deletes the database
+# "$@" passes extra arguments to subcommand build-test-db
+
+DB_FILE=CreeDictionary/test_db.sqlite3
+if [ -f "$DB_FILE" ]; then
+  rm -i $DB_FILE
+fi
+
+pipenv run python CreeDictionary/manage.py migrate API 0005 && manage-db build-test-db "$@"
--- libexec/import.sh
+++ libexec/import.sh
@@ -1,2 +1,11 @@
+# expected to be called from pipfile
 # 0005 was the basis of the import script in Database Manager
-rm CreeDictionary/test_db.sqlite3 && pipenv migrate API 0005 && manage-db build-test-db
+# -i makes it interactive, ask user to comfirm before he deletes the old test database
+# "$@" passes additional arguments to the shell script
+DB_FILE=CreeDictionary/db.sqlite3
+if [ -f "$DB_FILE" ]; then
+  echo "shit"
+  rm -i $DB_FILE
+fi
+
+pipenv migrate API 0005 && manage-db import "$@"
