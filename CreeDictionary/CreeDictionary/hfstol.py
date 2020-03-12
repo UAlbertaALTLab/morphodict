9d20ba4fdaf32e700add469a952b4ae69ea0a332
Eddie Antonio Santos <easantos@ualberta.ca>
2020-03-11 21:05:26 -0600

    Messy implementation of hfstol.

---
+++ CreeDictionary/CreeDictionary/hfstol.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+"""
+Temporary HFSTOL.
+"""
+
+import shutil
+from contextlib import contextmanager
+from subprocess import DEVNULL, check_output
+from tempfile import TemporaryFile
+from typing import IO, Generator, NamedTuple
+
+from utils.shared_res_dir import shared_res_dir as res
+
+# Ensure we can load everything!
+_analyzer_path = res / "fst" / "crk-descriptive-analyzer.hfstol"
+assert _analyzer_path.exists()
+_hfstol_bin = shutil.which("hfst-optimized-lookup")
+if _hfstol_bin is None:
+    raise ImportError("Cannot find hfst-optimized-lookup! Is it installed?")
+else:
+    _hfstol = _hfstol_bin
+
+
+class Analysis(NamedTuple):
+    lemma: str
+
+
+@contextmanager
+def write_file(text: str) -> Generator[IO[str], None, None]:
+    with TemporaryFile("w+", encoding="UTF-8") as tmp:
+        tmp.write(text)
+        tmp.flush()
+        tmp.seek(0)
+        yield tmp
+
+
+def analyze(wordform: str) -> Analysis:
+    with write_file(f"{wordform}\n") as input_file:
+        output = check_output(
+            [_hfstol, "-q", _analyzer_path],
+            stdin=input_file,
+            stderr=DEVNULL,
+            encoding="UTF-8",
+        )
+
+    *raw_analyses, blank_line = output.split("\n")
+    assert blank_line.strip() == ""
+
+    for line in raw_analyses:
+        input_form, _tab, analysis = line.partition("\t")
+        lemma, _plus, _rest = analysis.partition("+")
+        return Analysis(lemma=lemma)
+    raise NotImplementedError
