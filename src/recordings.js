6dc4b23948a92a1d9490c643443ba3094219149a
Eddie Antonio Santos <easantos@ualberta.ca>
2020-06-03 10:35:59 -0600

    style: sick eslint on recordings.js

--- src/recordings.js
+++ src/recordings.js
@@ -1,5 +1,5 @@
 // the specific URL for a given wordform (refactored from previous commits).
-const BASE_URL = 'https://sapir.artsrn.ualberta.ca/validation';
+const BASE_URL = 'https://sapir.artsrn.ualberta.ca/validation'
 
 export function fetchRecordings(wordform) {
   // TODO: should come from config.
@@ -20,48 +20,48 @@ export async function fetchFirstRecordingURL(wordform) {
  */
 export function retrieveListOfSpeakers() {
   // get the value of the wordform from the page
-  let wordform = document.getElementById('data:head').value;
-  let derivedURL = `${BASE_URL}/recording/_search/${wordform}`;
+  let wordform = document.getElementById('data:head').value
+  let derivedURL = `${BASE_URL}/recording/_search/${wordform}`
 
   // setting up the JSON request
-  let xhttp = new XMLHttpRequest();
-  xhttp.open('GET', derivedURL, true);
+  let xhttp = new XMLHttpRequest()
+  xhttp.open('GET', derivedURL, true)
 
   // receiving request information from SAPIR
   xhttp.onload = function() {
-    let returnedData = JSON.parse(this.response); // response from the server
-    let numberOfRecordings = returnedData.length; // number of records on the server
-    let recordingsList = document.querySelector('.recordings-list');
-    let recordingsHeading = document.querySelector('.definition__recordings');
-    
+    let returnedData = JSON.parse(this.response) // response from the server
+    let numberOfRecordings = returnedData.length // number of records on the server
+    let recordingsList = document.querySelector('.recordings-list')
+    let recordingsHeading = document.querySelector('.definition__recordings')
+
     // we only want to display our list of speakers once!
     if (recordingsList.childElementCount < numberOfRecordings) {
-      displaySpeakerList(returnedData);
+      displaySpeakerList(returnedData)
     }
 
     // the function that displays an individual speaker's name
     function displaySpeakerList(firstJSONData) {
-      recordingsHeading.insertAdjacentHTML('afterbegin', '<h3 class="explainer">Select a name below to hear the word above said by different speakers.</h3>');
-      
-      let speakerURLIndexCount = 0;
+      recordingsHeading.insertAdjacentHTML('afterbegin', '<h3 class="explainer">Select a name below to hear the word above said by different speakers.</h3>')
+
+      let speakerURLIndexCount = 0
 
       while (speakerURLIndexCount < numberOfRecordings) {
         // create a list element and set an attribute on it for testing
-        let individualSpeaker = document.createElement('li');
-        individualSpeaker.classList.add('recordings-list__item');
-        individualSpeaker.setAttribute('data-cy', 'recordings-list__item');
+        let individualSpeaker = document.createElement('li')
+        individualSpeaker.classList.add('recordings-list__item')
+        individualSpeaker.setAttribute('data-cy', 'recordings-list__item')
 
         // create a button element: add a class to it for future styling needs
-        let speakerButton = document.createElement('button');
-        speakerButton.classList.add('audio-snippet');
+        let speakerButton = document.createElement('button')
+        speakerButton.classList.add('audio-snippet')
 
         // put the button into the list
-        individualSpeaker.appendChild(speakerButton);
+        individualSpeaker.appendChild(speakerButton)
 
         // put the list into the DOM
-        recordingsList.appendChild(individualSpeaker);
+        recordingsList.appendChild(individualSpeaker)
 
-        speakerURLIndexCount++;
+        speakerURLIndexCount++
       }
 
       // TODOkobe: hey future Eddie (+ Kobe), should the for-loop be within the while loop above?
@@ -72,24 +72,24 @@ export function retrieveListOfSpeakers() {
       for (let speakerURLIndexCount = 0; speakerURLIndexCount < firstJSONData.length; speakerURLIndexCount++) {
 
         // select for the buttons...
-        let createdSpeakerButton = document.querySelectorAll('button.audio-snippet');
+        let createdSpeakerButton = document.querySelectorAll('button.audio-snippet')
 
         // ...and then iterate through them to add text
-        createdSpeakerButton[speakerURLIndexCount].innerText = firstJSONData[speakerURLIndexCount].speaker_name + ', Maskwacîs';
+        createdSpeakerButton[speakerURLIndexCount].innerText = firstJSONData[speakerURLIndexCount].speaker_name + ', Maskwacîs'
 
         // put an event listener on the button: the event is the URL playback
         createdSpeakerButton[speakerURLIndexCount].addEventListener('click', function() {
-          var audio = new Audio(firstJSONData[speakerURLIndexCount].recording_url);
-          audio.type = 'audio/m4a';
-          audio.play();
+          var audio = new Audio(firstJSONData[speakerURLIndexCount].recording_url)
+          audio.type = 'audio/m4a'
+          audio.play()
 
-        });
+        })
       }
     }
   }
 
   // send the request!
-  xhttp.send();
+  xhttp.send()
 }
 
 // TODOkobe: Once everything is working, play with a way to dynamically indicate (on the button) that a repeat 'speaker' is a v1, v2, v3, etc
