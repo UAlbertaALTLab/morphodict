eeda9ed9eea0ab7c0767ce17a583c4f749745d89
Eddie Antonio Santos <easantos@ualberta.ca>
2020-06-04 08:10:17 -0600

    change: remove reliance on jQuery in index.js
    Moves remaining jQuery stuff to tooltip.js

--- src/tooltip.js
+++ src/tooltip.js
@@ -1,11 +1,16 @@
 // adapted from poppers.js tutorial https://popper.js.org/docs/v2/tutorial/
 import {createPopper} from '@popperjs/core/dist/esm/popper'
 
+import $ from 'jquery'
 
 let popperInstance = null
 
+/**
+ * @param {Element} icon
+ * @param {Element} popup
+ */
 function create(icon, popup) {
-  popperInstance = createPopper(icon.get(0), popup.get(0), {
+  popperInstance = createPopper(icon, popup, {
     modifiers: [
       {
         name: 'offset',
@@ -30,20 +35,20 @@ const hideEvents = ['mouseleave', 'blur']
 /**
  * prepare the popup and the icon. Attach relevant handlers
  *
- * @param icon {jQuery}: icon that triggers the popup on hover/click/focus
- * @param popup {jQuery}: the popup that shows up
+ * @param icon {Element}: icon that triggers the popup on hover/click/focus
+ * @param popup {Element}: the popup that shows up
  */
 export function createTooltip(icon, popup) {
   showEvents.forEach(event => {
-    icon.on(event, () => {
-      popup.attr('data-show', '')
+    $(icon).on(event, () => {
+      popup.setAttribute('data-show', '')
       create(icon, popup)
     })
   })
 
   hideEvents.forEach(event => {
-    icon.on(event, () => {
-      popup.removeAttr('data-show')
+    $(icon).on(event, () => {
+      popup.removeAttribute('data-show')
       destroy()
     })
   })
