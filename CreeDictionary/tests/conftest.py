8805d0fc5c45e44115e1d416c41b1f3273f06ddd
Eddie Antonio Santos <easantos@ualberta.ca>
2020-06-11 20:35:51 -0600

    Revert "I don't know why this doesn't work"
    This reverts commit 2f6c23204b3a8fc068e01e73de7b7b5973af2b13.

--- CreeDictionary/tests/conftest.py
+++ CreeDictionary/tests/conftest.py
@@ -1,14 +1,13 @@
 from datetime import timedelta
 from os.path import dirname
 from pathlib import Path
-from typing import Sequence
 
 import pytest
 from API.models import Wordform
 from DatabaseManager.xml_importer import import_xmls
 from django.core.management import call_command
 from hypothesis import assume, settings
-from hypothesis.strategies import sampled_from
+from hypothesis.strategies import SearchStrategy, composite, integers, sampled_from
 
 settings.register_profile("default", deadline=timedelta(milliseconds=5000))
 # otherwise it's possible to get DeadlineExceed exception cuz each test function runs too long
@@ -23,35 +22,37 @@ def topmost_datadir():
     return Path(dirname(__file__)) / "data"
 
 
-class LazyWordforms(Sequence[Wordform]):
+class WordformStrategy(SearchStrategy[Wordform]):
     """
-    Fetches wordforms only when needed.
+    Strategy that fetches Wordform objects from the database LAZILY.
 
-    The query isn't executed until Hypothesis thinks it's a good idea to do
-    so.
+    The query isn't executed until the first example requested.
+
+    NOTE: This is NOT reproducible given a random seed,
+    because of its reliance on the database engine's random sort.
     """
 
     def __init__(self, **filter_args):
-        self._query_set = Wordform.objects.filter(**filter_args)
+        # random elements:
+        self._query_set = Wordform.objects.filter(**filter_args).order_by("?")
 
-    @property
-    def result_set(self) -> tuple:
-        if not hasattr(self, "_result_set"):
-            self._result_set = tuple(self._query_set)
-        return self._result_set
+    def _next(self):
+        if not hasattr(self, "_iterator"):
+            self._iterator = iter(self._query_set)
+        return next(self._iterator)
 
-    def __getitem__(self, index) -> Wordform:
-        return self.result_set[index]
+    def do_draw(self, data) -> Wordform:
+        return self._next()
 
-    def __len__(self) -> int:
-        return len(self.result_set)
+    def calc_is_cacheable(self, recur):
+        return False
 
 
 def lemmas():
     """
     Strategy to return lemmas from the database.
     """
-    return sampled_from(LazyWordforms(is_lemma=True, as_is=False))
+    return WordformStrategy(is_lemma=True, as_is=False)
 
 
 def migrate_and_import(dictionary_dir):
