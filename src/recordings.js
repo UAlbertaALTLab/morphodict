50ca55060706d813addabd54fcdd3aa7c758205e
Kobe Amoh <kobexamoh@gmail.com>
2020-06-17 04:13:14 -0600

    Generate test popup.

--- src/recordings.js
+++ src/recordings.js
@@ -1,5 +1,13 @@
 import SimpleTemplate from './simple-template.js'
 
+// popup for the speaker popup
+import {createPopper, popperGenerator} from '@popperjs/core/dist/esm/popper';
+const button = document.querySelector('.definition__icon definition-title__play-button'); // triggers the popup
+const tooltip = document.querySelector('#tooltip');
+
+Popper.createPopper(button, tooltip);
+
+
 // the specific URL for a given wordform (refactored from previous commits).
 // TODO: should come from config.
 const BASE_URL = 'https://sapir.artsrn.ualberta.ca/validation'
