b622a888b5dc8eddc889c6e5df7b327093081ccd
Eddie Antonio Santos <easantos@ualberta.ca>
2020-06-26 14:33:12 -0600

    test: parametrize orthography integration tests

--- CreeDictionary/tests/test_usage_of_morphodict.py
+++ CreeDictionary/tests/test_usage_of_morphodict.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
+"""
+Integration test of settings.py and morphodict application.
+"""
+
+import pytest
+
 from morphodict.orthography import ORTHOGRAPHY
 
 
 def test_morphodict_orthography():
     assert ORTHOGRAPHY.default == "Latn"
     assert {"Latn", "Cans", "Latn-x-macron"} <= set(ORTHOGRAPHY.available)
-    assert "SRO" in ORTHOGRAPHY.name_of("Latn")
-    assert "SRO" in ORTHOGRAPHY.name_of("Latn-x-macron")
-    assert "Syllabics" in ORTHOGRAPHY.name_of("Cans")
+
+
+@pytest.mark.parametrize(
+    "code,name", [("Latn", "SRO"), ("Latn-x-macron", "SRO"), ("Cans", "Syllabics"),]
+)
+def test_each_orthography(code, name):
+    assert name in ORTHOGRAPHY.name_of(code)
