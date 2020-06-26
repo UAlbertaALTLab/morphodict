0079b998500feaa950daf82bb8a7e65a6ba45c66
Eddie Antonio Santos <easantos@ualberta.ca>
2020-06-26 12:53:27 -0600

    test: Orthography.name_of()

--- CreeDictionary/tests/test_usage_of_morphodict.py
+++ CreeDictionary/tests/test_usage_of_morphodict.py
@@ -7,3 +7,6 @@ from morphodict.orthography import ORTHOGRAPHY
 def test_morphodict_orthography():
     assert ORTHOGRAPHY.default == "Latn"
     assert {"Latn", "Cans", "Latn-x-macron"} <= set(ORTHOGRAPHY.available)
+    assert "SRO" in ORTHOGRAPHY.name_of("Latn")
+    assert "SRO" in ORTHOGRAPHY.name_of("Latn-x-macron")
+    assert "Syllabics" in ORTHOGRAPHY.name_of("Cans")
