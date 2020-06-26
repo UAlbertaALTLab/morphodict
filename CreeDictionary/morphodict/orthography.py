ec9b7c8f175c3838434f239f1b7f393f618ff4eb
Eddie Antonio Santos <easantos@ualberta.ca>
2020-06-26 14:46:48 -0600

    feat: implement converter pseudo-dict

--- CreeDictionary/morphodict/orthography.py
+++ CreeDictionary/morphodict/orthography.py
@@ -5,12 +5,25 @@
 Handling of the writing system of the language.
 """
 
-from typing import Set
+from importlib import import_module
+from typing import Callable, Set
 
 from django.conf import settings
 
 
 class Orthography:
+    class _Converter:
+        def __getitem__(self, code: str) -> Callable[[str], str]:
+            path = settings.MORPHODICT_ORTHOGRAPHY["available"].get("converter", None)
+            if path is None:
+                return lambda text: text
+
+            *module_path, callable_name = path.split(".")
+            module = import_module(".".join(module_path))
+            return getattr(module, callable_name)
+
+    converter = _Converter()
+
     @property
     def default(self) -> str:
         return settings.MORPHODICT_ORTHOGRAPHY["default"]
