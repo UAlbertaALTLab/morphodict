93e6952986779db2b482ce8b13df798de866fb78
matt <syan4@ualberta.ca>
2020-11-25 11:52:17 -0700

    no affix search for click-in-text

--- CreeDictionary/tests/API_tests/test_views.py
+++ CreeDictionary/tests/API_tests/test_views.py
@@ -36,3 +36,24 @@ def test_click_in_text_no_params():
     response = c.get(reverse("cree-dictionary-word-click-in-text-api"))
 
     assert response.status_code == 400
+
+
+@pytest.mark.django_db
+def test_click_in_text_no_affix_search():
+    # This test guarantees affix search is turned off in click-in-text API
+
+    # Should affix search be turned on for click-in-tex, we would have a full page of results
+    # for the case of wapamew and a lot of results are from prefix/suffix search
+    #
+    # The user might not be certain what they are looking for on Itwewina, thus the need
+    # for relaxation and affix search. While click-in-text users searches for some exact cree they find on the web.
+    # So prefix/suffix search is unnecessary.
+    c = Client()
+
+    ascii_wapamew = "wapamew"
+    suffix_search_result = "asawâpamêw"
+    normal_search_response = c.get(reverse("cree-dictionary-search") + f"?q={ascii_wapamew}").content.decode("utf-8")
+    click_in_text_response = c.get(reverse("cree-dictionary-word-click-in-text-api") + f"?q={ascii_wapamew}").content.decode(
+        "utf-8")
+
+    assert suffix_search_result in normal_search_response and suffix_search_result not in click_in_text_response
