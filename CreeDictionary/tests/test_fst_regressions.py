95a2fddc108d30940f5238f71486ce0d5840a8f4
Eddie Antonio Santos <easantos@ualberta.ca>
2020-12-09 16:32:26 -0700

    test: fst regressions

---
+++ CreeDictionary/tests/test_fst_regressions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import CreeDictionary.hfstol as temp_hfstol
+
+# TODO: use better imports https://github.com/UAlbertaALTLab/cree-intelligent-dictionary/issues/525
+from utils.data_classes import Analysis
+
+
+def test_fst_analysis():
+    analyses = set(temp_hfstol.analyze("ta-pe-kiwemakaniyiw"))
+    assert (
+        Analysis(
+            raw_prefixes="PV/ta+PV/pe", lemma="kîwêmakan", raw_suffixes="V+II+Ind+4Sg"
+        )
+        in analyses
+    )
+
+
+def test_fst_generation():
+    wordforms = set(temp_hfstol.generate("PV/ta+PV/pe+kîwêmakan+V+II+Ind+4Sg"))
+    assert "ta-pê-kîwêmakaniyiw" in wordforms
