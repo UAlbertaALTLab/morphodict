644e2ff1b3ed81bca7f0440c3e0081a8cb262cd0
matt <syan4@ualberta.ca>
2020-12-21 16:15:17 -0700

    extract project_path utility

--- CreeDictionary/utils/__init__.py
+++ CreeDictionary/utils/__init__.py
@@ -2,3 +2,4 @@ from .cree_lev_dist import get_modified_distance
 from .enums import *
 from .shared_res_dir import shared_res_dir
 from .vars import *
+from .project_path import get_project_path, PROJECT_ROOT
