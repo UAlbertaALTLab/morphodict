7f3431c8b2b7b3b6a48afd79ac925818c226ff38
Eddie Antonio Santos <eddieantonio@hey.com>
2021-01-11 15:14:01 -0700

    refactor(test): factor out interactive tests

--- cypress/integration/search.spec.js
+++ cypress/integration/search.spec.js
@@ -1,28 +1,4 @@
 context('Searching', () => {
-  describe('I want to see search results showing dynamically while I type', () => {
-    // https://github.com/UAlbertaALTLab/cree-intelligent-dictionary/issues/120
-    it('should search for first minos, then minosis', () => {
-      cy.visit('/')
-      cy.search('minos')
-        .searchResultsContain('cat')
-
-      // makes it minos+is = minosis
-      cy.search('is')
-        .searchResultsContain('kitten')
-    })
-  })
-
-  describe('I want to see search results showing immediately after pressing enter', () => {
-    // https://github.com/UAlbertaALTLab/cree-intelligent-dictionary/issues/120
-    it('shows results for minosis immediately after enter is pressed', () => {
-      cy.visit('/')
-      cy.search('minosis', {pressEnter: true})
-        .searchResultsContain('kitten')
-
-    })
-  })
-
-
   describe('I want to see search results by the url', () => {
     it('perform the search by going directly to the URL', () => {
       cy.visitSearch('minos')
