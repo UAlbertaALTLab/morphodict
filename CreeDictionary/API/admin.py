29065d9b7e4eaf0cea79c6fee23f73ad5aa8168b
Eddie Antonio Santos <eddieantonio@hey.com>
2021-01-12 21:52:27 -0700

    style: reformat admin.py
    [skip ci] [skip reformat]

--- CreeDictionary/API/admin.py
+++ CreeDictionary/API/admin.py
@@ -80,9 +80,9 @@ class InflectionAdmin(admin.ModelAdmin):
         search_term = search_term.lower()
 
         # utilize the spell relax in descriptive_analyzer
-        fst_analyses: Set[str] = expensive.descriptive_analyzer.feed_in_bulk_fast([search_term])[
-            search_term
-        ]
+        fst_analyses: Set[str] = expensive.descriptive_analyzer.feed_in_bulk_fast(
+            [search_term]
+        )[search_term]
         unfiltered_queryset = Wordform.objects.filter(analysis__in=fst_analyses)
         unfiltered_queryset |= Wordform.objects.filter(text=search_term)
         return unfiltered_queryset & queryset, True
