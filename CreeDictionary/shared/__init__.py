7e9f2c4f2c9385aac72b389ad7494b583cb397f7
Eddie Antonio Santos <eddieantonio@hey.com>
2021-01-12 20:52:11 -0700

    refactor: move HFSTOLWithoutFragmentAnalyses to workaround

--- CreeDictionary/shared/__init__.py
+++ CreeDictionary/shared/__init__.py
@@ -4,40 +4,12 @@ shared (expensive) instances
 
 from typing import Dict, Iterable, Set, Tuple
 
-from hfstol import HFSTOL
 from utils import shared_res_dir
 
-from .expensive import paradigm_filler
+from .expensive import normative_generator, paradigm_filler
+from .workaround import HFSTOLWithoutFragmentAnalyses
 
 _fst_dir = shared_res_dir / "fst"
-
-
-class HFSTOLWithoutFragmentAnalyses:
-    """
-    Acts like HFSTOL but removes analyses with +Err/Frag.
-    """
-
-    def __init__(self, transducer: HFSTOL):
-        self._hfstol = transducer
-
-    @staticmethod
-    def _is_good_analysis(analysis: str) -> bool:
-        return "+Err/Frag" not in analysis
-
-    def feed_in_bulk_fast(
-        self, surface_forms: Iterable[str], multi_process: int = 1
-    ) -> Dict[str, Set[str]]:
-        original = self._hfstol.feed_in_bulk_fast(
-            surface_forms, multi_process=multi_process
-        )
-        return {
-            wordform: {a for a in analyses if self._is_good_analysis(a)}
-            for wordform, analyses in original.items()
-        }
-
-    @classmethod
-    def from_file(cls, path) -> HFSTOL:
-        return cls(HFSTOL.from_file(path))
 
 
 descriptive_analyzer = HFSTOLWithoutFragmentAnalyses.from_file(
@@ -47,5 +19,3 @@ descriptive_analyzer = HFSTOLWithoutFragmentAnalyses.from_file(
 strict_analyzer = HFSTOLWithoutFragmentAnalyses.from_file(
     _fst_dir / "crk-strict-analyzer.hfstol"
 )
-
-normative_generator = HFSTOL.from_file(_fst_dir / "crk-normative-generator.hfstol")
--- CreeDictionary/shared/expensive.py
+++ CreeDictionary/shared/expensive.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
+from hfstol import HFSTOL
 from utils import paradigm_filler as pf
+from utils import shared_res_dir
+
+_fst_dir = shared_res_dir / "fst"
 
 paradigm_filler = pf.ParadigmFiller.default_filler()
+
+normative_generator = HFSTOL.from_file(_fst_dir / "crk-normative-generator.hfstol")
