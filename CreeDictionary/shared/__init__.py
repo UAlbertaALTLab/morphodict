1cc9f957a00967f80e71f0805c6f48593f7e06b5
Eddie Antonio Santos <eddieantonio@hey.com>
2021-01-12 20:27:22 -0700

    refactor: extract constant: _fst_dir

--- CreeDictionary/shared/__init__.py
+++ CreeDictionary/shared/__init__.py
@@ -8,6 +8,8 @@ from hfstol import HFSTOL
 from utils import paradigm_filler as pf
 from utils import shared_res_dir
 
+_fst_dir = shared_res_dir / "fst"
+
 
 class FilteredHFSTOL:
     """
@@ -39,14 +41,11 @@ class FilteredHFSTOL:
 
 paradigm_filler = pf.ParadigmFiller.default_filler()
 
+
 descriptive_analyzer = FilteredHFSTOL.from_file(
-    shared_res_dir / "fst" / "crk-descriptive-analyzer.hfstol"
+    _fst_dir / "crk-descriptive-analyzer.hfstol"
 )
 
-strict_analyzer = FilteredHFSTOL.from_file(
-    shared_res_dir / "fst" / "crk-strict-analyzer.hfstol"
-)
+strict_analyzer = FilteredHFSTOL.from_file(_fst_dir / "crk-strict-analyzer.hfstol")
 
-normative_generator = HFSTOL.from_file(
-    shared_res_dir / "fst" / "crk-normative-generator.hfstol"
-)
+normative_generator = HFSTOL.from_file(_fst_dir / "crk-normative-generator.hfstol")
