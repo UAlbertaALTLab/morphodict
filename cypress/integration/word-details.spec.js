37dcd78b656d3f5beb30c4cea7b6450b846bd40b
Eddie Antonio Santos <eddieantonio@hey.com>
2021-01-15 16:42:40 -0700

    test: show wordclass and inflectional category

--- cypress/integration/word-details.spec.js
+++ cypress/integration/word-details.spec.js
@@ -3,27 +3,33 @@ context('Word details', () => {
     // Test at least one word from each word class:
     //
     const testCases = [
-      {wc: 'VTA', word: 'mowêw'},
-      {wc: 'VAI', word: 'wâpiw'},
-      {wc: 'VTI', word: 'mîcisow'},
-      {wc: 'VII', word: 'nîpin'},
-      {wc: 'NAD', word: 'nôhkom'},
-      {wc: 'NID', word: 'mîpit'},
-      {wc: 'NA', word: 'minôs'},
-      {wc: 'NI', word: 'nipiy'},
-      {wc: 'IPC', word: 'ispîhk'},
+      {wc: 'VTA', ic: 'VTA-1', word: 'mowêw'},
+      {wc: 'VAI', ic: 'VAI-v', word: 'wâpiw'},
+      {wc: 'VTI', ic: 'VTI-3', word: 'mîciw'},
+      {wc: 'VII', ic: 'VII-n', word: 'nîpin'},
+      {wc: 'NAD', ic: 'NDA-1', word: 'nôhkom'},
+      {wc: 'NID', ic: 'NDI-1', word: 'mîpit'},
+      {wc: 'NA',  ic: 'NA-1', word: 'minôs'},
+      {wc: 'NI',  ic: 'NI-2', word: 'nipiy'},
+      {wc: 'IPC', ic: null, word: 'ispîhk'},
     ]
 
     // Create test cases for each word above
-    for (let {wc, word} of testCases) {
+    for (let {wc, word, ic} of testCases) {
       it(`should display the word class and inflection class for ${word} (${wc})`, () => {
         cy.visitLemma(word)
 
         cy.url()
           .should('contain', 'word/')
 
-        // TODO: assert that we can see the word class and inflectional
-        // category!
+        cy.get('[data-cy=elaboration]')
+          .contains(wc)
+
+        if (!ic)
+          return
+
+        cy.get('[data-cy=elaboration]')
+          .contains(ic)
       })
     }
   })
