3ed0d7af3020d1af26b35e223c48ec7e41cadc40
Eddie Antonio Santos <eddieantonio@hey.com>
2021-01-17 15:09:39 -0700

    change: make the verbose labels more closely match the search labels

--- cypress/integration/word-details.spec.js
+++ cypress/integration/word-details.spec.js
@@ -7,8 +7,10 @@ context('Word details', () => {
       {wc: 'VAI', ic: 'VAI-v', word: 'wâpiw'},
       {wc: 'VTI', ic: 'VTI-3', word: 'mîciw'},
       {wc: 'VII', ic: 'VII-n', word: 'nîpin'},
-      {wc: 'NAD', ic: 'NDA-1', word: 'nôhkom'},
-      {wc: 'NID', ic: 'NDI-1', word: 'mîpit'},
+      // TODO: pretty sure this should be NAD, but the labels say otherwise:
+      {wc: 'NDA', ic: 'NDA-1', word: 'nôhkom'},
+      // TODO: pretty sure this should be NID, but the labels say otherwise:
+      {wc: 'NDI', ic: 'NDI-1', word: 'mîpit'},
       {wc: 'NA',  ic: 'NA-1', word: 'minôs'},
       {wc: 'NI',  ic: 'NI-2', word: 'nipiy'},
       {wc: 'IPC', ic: null, word: 'ispîhk'},
