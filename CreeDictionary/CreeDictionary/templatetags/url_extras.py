5e48e9138703df1945ecc23bb71102e6b92cfa39
Eddie Antonio Santos <eddieantonio@hey.com>
2021-02-12 15:20:16 -0700

    change: more thorough treatment or URLs
    It turns out URLs are REALLY messy, so this delegates to Python's default parser and makes our assumptions VERY clear.

--- CreeDictionary/CreeDictionary/templatetags/url_extras.py
+++ CreeDictionary/CreeDictionary/templatetags/url_extras.py
@@ -1,7 +1,8 @@
 """
 Get absolute URLs for static assets.
 """
-from typing import Optional
+
+from urllib.parse import ParseResult, urlparse, urlunparse
 
 from django import template
 from django.templatetags.static import StaticNode
@@ -10,35 +11,40 @@ from django.urls import reverse
 register = template.Library()
 
 
-class AbsoluteURLStaticNode(StaticNode):
+class AbstaticNode(StaticNode):
     """
-    Like Django's {% static %} tag, but always returns an absolute URL.
+    {% abstatic %} is like Django's {% static %} tag,
+    but always returns an absolute URI.
     """
 
     def url(self, context) -> str:
-        component = super().url(context)
-        if is_absolute_url(component):
-            return component
+        url_to_asset = super().url(context)
 
-        # Django say "absolute_uri" but they really mean "absolute_url" ;)
-        return context["request"].build_absolute_uri(component)
+        parsed_url = urlparse(url_to_asset)
+        assert parsed_url.path
+
+        if is_absolute_uri(parsed_url):
+            return url_to_asset
+
+        # Delegate to Django to provide its own schema and authority:
+        path_and_file = urlunparse(parsed_url._replace(scheme="", netloc=""))
+        return context["request"].build_absolute_uri(path_and_file)
 
 
-def is_absolute_url(component: str) -> bool:
+def is_absolute_uri(url: ParseResult) -> bool:
     """
     What is an absolute URL?
 
     From: https://stackoverflow.com/a/17407021/6626414
 
     > Absolute URLs contain a great deal of information which may already be known from
-    > the context of the base document's retrieval, including the scheme, network 
+    > the context of the base document's retrieval, including the scheme, network
     > location, and parts of the URL path.
 
     See also: https://tools.ietf.org/html/rfc1808
     """
-    if not component.startswith("/"):
-        assert component.startswith("http"), f"Not an absolute URL: {component!r}"
-        assert ":" in component, f"Not an absolute URL: {component!r}"
+    # netloc == authority, i.e., [username[:password]@]example.com[:443]
+    if url.scheme and url.netloc:
         return True
     return False
 
@@ -51,4 +57,4 @@ def abstatic(parser, token):
 
     Derived from: https://github.com/django/django/blob/635d53a86a36cde7866b9caefeb64d809e6bfcd9/django/templatetags/static.py#L143-L159
     """
-    return AbsoluteURLStaticNode.handle_token(parser, token)
+    return AbstaticNode.handle_token(parser, token)
