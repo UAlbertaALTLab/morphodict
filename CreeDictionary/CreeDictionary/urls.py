ed8732d1cb6f2e872ad4c8822b41dc4dfec2ec62
Eddie Antonio Santos <eddieantonio@hey.com>
2021-02-12 19:21:09 -0700

    refactor: convert hacky urlpatterns to idiomatic urlpatterns

--- CreeDictionary/CreeDictionary/urls.py
+++ CreeDictionary/CreeDictionary/urls.py
@@ -2,7 +2,6 @@
 Definition of urls for CreeDictionary.
 """
 
-import logging
 import os
 
 import API.views as api_views
@@ -16,114 +15,64 @@ from django_js_reverse.views import urls_js
 
 from CreeDictionary import views
 
-logger = logging.getLogger(__name__)
-
-# 2019/May/21 Matt Yan:
-
-# The reason to have different rules in development/production:
-
-# static file urls / web-page urls / API urls in this project all begin with "cree-dictionary"
-# so that in production on server sapir, the cree-dictionary service can be proxy-ed by looking for
-# initial "cree-dictionary" in the url.
-
-# example url:
-# http://sapir.artsrn.ualberta.ca/cree-dictionary/search/hello
-
-# in development, though, the initial "cree-dictionary" is not needed, example:
-# http://localhost:8000/search/hello
-# Note: re_path here, for example "re_path("^(cree-dictionary/)?some/url")", isn't a good solution. It messes up with
-# url reversion
-
-# 2021-01-12 Eddie Antonio Santos:
-#
-# I'm in the process of fixing this on Sapir.
-#
-# This routing code needs to know how to handle both when SCRIPT_NAME is
-# specified as '/cree-dictionary' and when it's not.
-#
-# Then, the Gunicorn config on Sapir must be changed to supply -v
-# SCRIPT_NAME='/cree-dictionary' in its startup options.
-#
-# The following function, as well as any comments ands hacks associated with it can be
-# deleted soon!
-
-
-def running_on_sapir_without_script_name():  # pragma: no cover
-    """
-    No longer required.
-    """
-    return False
-
-
-# TODO: Convert this to an idiomatic Django style when we drop support for
-# Sapir
-_urlpatterns = [
+urlpatterns = [
     # user interface
-    ("", views.index, "cree-dictionary-index"),
-    ("search", views.index, "cree-dictionary-search"),
+    path("", views.index, name="cree-dictionary-index"),
+    path("search", views.index, name="cree-dictionary-search"),
     # DEPRECATED: this route 👇 is a permanent redirect to the route above ☝️
-    (
+    path(
         "search/<str:query_string>/",
         views.redirect_search,
-        "cree-dictionary-index-with-query",
+        name="cree-dictionary-index-with-query",
     ),
     # word is a user-friendly alternative for the linguistic term "lemma"
-    (
+    path(
         "word/<str:lemma_text>/",
         views.lemma_details,
-        "cree-dictionary-index-with-lemma",
+        name="cree-dictionary-index-with-lemma",
     ),
-    ("about", views.about, "cree-dictionary-about"),
-    ("contact-us", views.contact_us, "cree-dictionary-contact-us"),
+    path("about", views.about, name="cree-dictionary-about"),
+    path("contact-us", views.contact_us, name="cree-dictionary-contact-us"),
     # internal use to render boxes of search results
-    (
+    path(
         "_search_results/<str:query_string>/",
         views.search_results,
-        "cree-dictionary-search-results",
+        name="cree-dictionary-search-results",
     ),
     # internal use to render paradigm and only the paradigm
-    (
+    path(
         "_paradigm_details/",
         views.paradigm_internal,
-        "cree-dictionary-paradigm-detail",
+        name="cree-dictionary-paradigm-detail",
     ),
     # cree word translation for click-in-text
-    (
+    path(
         "click-in-text/",
         api_views.click_in_text,
-        "cree-dictionary-word-click-in-text-api",
+        name="cree-dictionary-word-click-in-text-api",
     ),
-    ("admin/", admin.site.urls, "admin"),
-    (
+    path("admin/", admin.site.urls),
+    path(
         "",
         include("morphodict.urls"),
-        "cree-dictionary-change-orthography",
+        name="cree-dictionary-change-orthography",
     ),
-    ("search-quality/", include("search_quality.urls"), "search_quality"),
+    path("search-quality/", include("search_quality.urls")),
 ]
 
 # Add style debugger, but only in DEBUG mode!
 if settings.DEBUG:
-    _urlpatterns.append(("styles", views.styles, "styles"))
+    urlpatterns.append(path("styles", views.styles, name="styles"))
 
-# XXX: ugly hack to make this work on a local instance and on Sapir
-urlpatterns = []
-
-for route, view, name in _urlpatterns:
-    # kwarg `name` for url reversion in html/py/js code
-    urlpatterns.append(path(route, view, name=name))
-
-# magic that allows us to reverse urls in js  https://github.com/ierror/django-js-reverse
+# Reverse URLs in JavaScript:  https://github.com/ierror/django-js-reverse
 urlpatterns.append(url(fr"^jsreverse/$", urls_js, name="js_reverse"))
-
-admin.site.site_url = "/"
 
 if settings.DEBUG:
     # saves the need to `manage.py collectstatic` in development
     urlpatterns += staticfiles_urlpatterns()
 
-    if settings.ENABLE_DJANGO_DEBUG_TOOLBAR:
-        import debug_toolbar
+if settings.DEBUG and settings.ENABLE_DJANGO_DEBUG_TOOLBAR:
+    import debug_toolbar
 
-        # necessary for debug_toolbar to work
-        urlpatterns.append(path("__debug__/", include(debug_toolbar.urls)))
+    # necessary for debug_toolbar to work
+    urlpatterns.append(path("__debug__/", include(debug_toolbar.urls)))
