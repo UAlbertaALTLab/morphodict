8601233988db3728062c7f64d5cdb97f9e299542
Eddie Antonio Santos <eddieantonio@hey.com>
2021-03-04 14:21:30 -0700

    change: use LOG_LEVEL for both gunicorn and django

--- CreeDictionary/gunicorn.conf.py
+++ CreeDictionary/gunicorn.conf.py
@@ -14,6 +14,8 @@ bind = f"0.0.0.0:8000"
 # Redirect output to errorlog
 capture_output = True
 
-loglevel = os.getenv("GUNICORN_LOG_LEVEL", "info")
+# Log level is documented as lowercase for some reason:
+# See: https://docs.gunicorn.org/en/stable/settings.html#loglevel
+loglevel = os.getenv("LOG_LEVEL", "info").lower()
 errorlog = "-"
 accesslog = "-"
