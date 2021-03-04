1e670c9b83ce9c5de8f8315d95ea0b9baecb43fb
Eddie Antonio Santos <eddieantonio@hey.com>
2021-03-04 15:22:34 -0700

    tempting fate we are tired

--- CreeDictionary/gunicorn.conf.py
+++ CreeDictionary/gunicorn.conf.py
@@ -14,6 +14,8 @@ bind = f"0.0.0.0:8000"
 # Redirect output to errorlog
 capture_output = True
 
+workers = 3
+
 # Log level is documented as lowercase for some reason:
 # See: https://docs.gunicorn.org/en/stable/settings.html#loglevel
 loglevel = os.getenv("LOG_LEVEL", "info").lower()
