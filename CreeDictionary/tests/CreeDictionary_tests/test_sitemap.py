da8b0a4d5a3a054f0a399cf2a64571896f896634
Eddie Antonio Santos <eddieantonio@hey.com>
2021-03-12 15:25:12 -0700

    test: can visit the sitemap.xml

---
+++ CreeDictionary/tests/CreeDictionary_tests/test_sitemap.py
@@ -0,0 +1,52 @@
+"""
+Test that the sitemap is accessible and has expected results.
+"""
+
+import random
+import xml.etree.ElementTree as ET
+from http import HTTPStatus
+from urllib.parse import urlparse
+
+import pytest
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+def test_can_access_sitemap(client, sitemap_url):
+    assert sitemap_url == "/sitemap.xml"
+
+    r = client.get(sitemap_url)
+    assert r.status_code == HTTPStatus.OK
+    assert "xml" in r["Content-Type"]
+
+
+@pytest.mark.django_db
+def test_sitemap_has_valid_locations(client, sitemap_url):
+    URLS_TO_SAMPLE = 10
+
+    r = client.get(sitemap_url)
+    root = ET.fromstring(r.content)
+    # Expecting the following:
+    # <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    #   <!-- primary site urls -->
+    #   <url>
+    #     <loc>//itwewina.altlab.app/</loc>
+    #   </url>
+    #
+    #   <!-- head word forms -->
+    #   <url>
+    #     <loc>//itwewina.altlab.app/</loc>
+    #   </url>
+    # </urlset>
+    ns = {"sitemap": "http://www.sitemaps.org/schemas/sitemap/0.9"}
+    locations = [loc.text for loc in root.findall("./sitemap:url/sitemap:loc", ns)]
+
+    # Try visiting a page:
+    url = random.choice(locations)
+    r = client.get(urlparse(url).path, follow=True)
+    assert r.status_code == HTTPStatus.OK, f"unexpected status for {url}"
+
+
+@pytest.fixture
+def sitemap_url():
+    return reverse("django.contrib.sitemaps.views.sitemap")
