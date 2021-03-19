d6a56ac663f4741f0d5c72414ac46215f01564d2
Eddie Antonio Santos <eddieantonio@hey.com>
2021-03-19 09:37:32 -0600

    refactor: attempt to make gross XML parsing easier to understand

--- CreeDictionary/tests/CreeDictionary_tests/test_sitemap.py
+++ CreeDictionary/tests/CreeDictionary_tests/test_sitemap.py
@@ -23,24 +23,12 @@ def test_can_access_sitemap(client, sitemap_url):
 @pytest.mark.django_db
 def test_sitemap_has_valid_locations(client, sitemap_url):
     r = client.get(sitemap_url)
+
     root = ET.fromstring(r.content)
-    # Expecting the following:
-    # <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    #   <!-- primary site urls -->
-    #   <url>
-    #     <loc>http://itwewina.altlab.app/</loc>
-    #   </url>
-    #
-    #   <!-- head word forms -->
-    #   <url>
-    #     <loc>http://itwewina.altlab.app/word/atim/</loc>
-    #   </url>
-    # </urlset>
-    ns = {"sitemap": "http://www.sitemaps.org/schemas/sitemap/0.9"}
-    locations = [loc.text for loc in root.findall("./sitemap:url/sitemap:loc", ns)]
+    urls = [loc.text for loc in find_all_location_elements(root)]
 
     # Try visiting a page:
-    url = random.choice(locations)
+    url = random.choice(urls)
     r = client.get(urlparse(url).path, follow=True)
     assert r.status_code == HTTPStatus.OK, f"unexpected status for {url}"
 
@@ -48,3 +36,34 @@ def test_sitemap_has_valid_locations(client, sitemap_url):
 @pytest.fixture
 def sitemap_url():
     return reverse("django.contrib.sitemaps.views.sitemap")
+
+
+def find_all_location_elements(root):
+    """
+    Given the following markup:
+
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+          <!-- primary site urls -->
+          <url>
+            <loc>http://itwewina.altlab.app/</loc>
+          </url>
+
+          <!-- head word forms -->
+          <url>
+            <loc>http://itwewina.altlab.app/word/atim/</loc>
+          </url>
+        </urlset>
+
+    Returns all <loc> elements:
+    <loc>http://itwewina.altlab.app/</loc>
+    <loc>http://itwewina.altlab.app/word/atim/</loc>
+    """
+    # The ElementTree API is... not great.
+    # Whenever I get an XML document and it uses namespaces,
+    # and I try to query it, I always forget that the tagname has to be QName
+    # like: <http://www.sitemaps.org/schemas/sitemap/0.9:url>
+    # or: <http://www.sitemaps.org/schemas/sitemap/0.9:loc>
+    # this namespace
+    namespace_aliases = {"sitemap": "http://www.sitemaps.org/schemas/sitemap/0.9"}
+    # XPath to return all <loc>s within all <url>s from the current node.
+    return root.findall("./sitemap:url/sitemap:loc", namespace_aliases)
