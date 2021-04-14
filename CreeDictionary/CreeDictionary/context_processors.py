73aaa2ce63bff3a9a03e499792bbcdb6e20bad17
Eddie Antonio Santos <eddieantonio@hey.com>
2021-04-14 13:36:29 -0600

    refactor: rename class to make it 'private'

--- CreeDictionary/CreeDictionary/context_processors.py
+++ CreeDictionary/CreeDictionary/context_processors.py
@@ -7,16 +7,6 @@ from django.http import HttpRequest
 from .display_options import DEFAULT_DISPLAY_MODE, DISPLAY_MODE_COOKIE, DISPLAY_MODES
 
 
-class Display:
-    def __init__(self, request: HttpRequest):
-        mode = request.COOKIES.get(DISPLAY_MODE_COOKIE)
-
-        if mode not in DISPLAY_MODES:
-            mode = DEFAULT_DISPLAY_MODE
-
-        self.mode = mode
-
-
 def display(request: HttpRequest):
     """
     Django context processor that augments the context with "display". "display" has the
@@ -24,4 +14,18 @@ def display(request: HttpRequest):
 
      - display.mode -- "basic" or "traditional"
     """
-    return {"display": Display(request)}
+    return {"display": _Display(request)}
+
+
+class _Display:
+    """
+    Provides the `display` template variable.
+    """
+
+    def __init__(self, request: HttpRequest):
+        mode = request.COOKIES.get(DISPLAY_MODE_COOKIE)
+
+        if mode not in DISPLAY_MODES:
+            mode = DEFAULT_DISPLAY_MODE
+
+        self.mode = mode
