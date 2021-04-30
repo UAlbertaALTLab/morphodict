0db2edba1b2284f85f5d2bf882c8925131f63880
Eddie Antonio Santos <eddieantonio@hey.com>
2021-04-30 15:26:56 -0600

    test: use test fixtures instead of production layouts

--- CreeDictionary/tests/CreeDictionary_tests/test_paradigm_manager.py
+++ CreeDictionary/tests/CreeDictionary_tests/test_paradigm_manager.py
@@ -1,11 +1,13 @@
+from pathlib import Path
+
+import pytest
 from more_itertools import first, ilen
 
-from CreeDictionary.paradigm.manager import default_paradigm_manager
+from CreeDictionary.paradigm.manager import ParadigmManager
 
 
-def test_generates_demonstrative_paradigm() -> None:
-    layouts = default_paradigm_manager()
-    paradigm = layouts.paradigm_for("niya+Pron+Pers+1Sg")
+def test_generates_demonstrative_paradigm(paradigm_manager) -> None:
+    paradigm = paradigm_manager.paradigm_for("niya+Pron+Pers+1Sg")
     assert paradigm is not None
     assert paradigm.contains_wordform("niya")
     assert paradigm.contains_wordform("kiya")
@@ -13,3 +15,14 @@ def test_generates_demonstrative_paradigm() -> None:
 
     assert ilen(paradigm.panes) == 1
     pane = first(paradigm.panes)
+
+
+@pytest.fixture
+def paradigm_manager(shared_datadir: Path) -> ParadigmManager:
+    from shared import expensive
+
+    layout_dir = shared_datadir / "paradigm-layouts"
+    assert layout_dir.is_dir()
+    assert (layout_dir / "static").is_dir()
+    assert (layout_dir / "dynamic").is_dir()
+    return ParadigmManager(layout_dir, expensive.strict_generator)
