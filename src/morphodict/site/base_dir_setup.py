278dfb9a1046b5e471526bbd17216a00020179e2
Andrew Neitsch <andrew@neitsch.ca>
2021-05-20 14:07:46 -0600

    Apply suggestions from code review
    Co-authored-by: Eddie Antonio Santos <eddieantonio@hey.com>

--- src/morphodict/site/base_dir_setup.py
+++ src/morphodict/site/base_dir_setup.py
@@ -9,13 +9,13 @@ _base_dir = None
 
 
 def get_base_dir():
-    assert _base_dir is not None
+    assert _base_dir is not None, "base_dir unset: set it with set_base_dir()"
     return _base_dir
 
 
 def set_base_dir(dir):
     global _base_dir
 
-    assert _base_dir is None
-    assert dir is not None
+    assert _base_dir is None, "base_dir is already set!"
+    assert dir.is_dir()
     _base_dir = dir
