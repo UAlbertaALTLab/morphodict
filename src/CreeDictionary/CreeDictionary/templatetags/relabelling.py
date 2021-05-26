606214da1eac9e953ad77b79ac29258c7793dad4
Eddie Antonio Santos <eddieantonio@hey.com>
2021-05-26 14:02:27 -0600

    change: log when a relabelling cannot be made

--- src/CreeDictionary/CreeDictionary/templatetags/relabelling.py
+++ src/CreeDictionary/CreeDictionary/templatetags/relabelling.py
@@ -2,18 +2,25 @@
 Access to relabelling from templates.
 """
 
+import logging
+
 from django import template
 
 from CreeDictionary.CreeDictionary.relabelling import LABELS
 from CreeDictionary.utils.types import FSTTag
 
+logger = logging.getLogger(__name__)
 register = template.Library()
 
 
 @register.simple_tag
-def relabel(thing: tuple[FSTTag]):
+def relabel(tags: tuple[FSTTag]):
     """
     Gets the best matching label for the given object.
     """
     # TODO: take in request context; relabel according to current label preference
-    return LABELS.english.get_longest(thing)
+    if label := LABELS.english.get_longest(tags):
+        return label
+
+    logger.warning("Could not find relabelling for tags: %r", tags)
+    return "+".join(tags)
