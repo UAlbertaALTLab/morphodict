5762d86edf6a5ad2436db1ade56e2e248e7bf507
Eddie Antonio Santos <eddieantonio@hey.com>
2021-05-28 15:00:39 -0600

    remove: postcss-custom-properties

--- frontend/css/postcss.config.js
+++ frontend/css/postcss.config.js
@@ -2,7 +2,6 @@
  * PostCSS config.
  */
 
-const postcssCustomProperties = require('postcss-custom-properties')
 const postcssImport = require('postcss-import')
 
 module.exports = {
@@ -13,25 +12,6 @@ module.exports = {
      * Inline @import rules (e.g., @import "normalize.css").
      */
     postcssImport(),
-    /**
-     * Turns CSS Custom Properties (a.k.a., variables) into old school
-     * declarations. e.g.,
-     *   :root {
-     *    --my-var: blue;
-     *   }
-     *   p {
-     *    color: var(--my-var);
-     *   }
-     *
-     * becomes
-     *
-     *  p {
-     *    color: blue;
-     *    color: var(--my-var);
-     *  }
-     */
-    postcssCustomProperties(),
-
     // NOTE: minification is handled by Rollup!
   ]
 }
