42e0bf098dc0dd81a80189993576c5ec954ac538
Eddie Antonio Santos <eddieantonio@hey.com>
2021-05-31 21:43:15 -0600

    style: reformat test_preference

--- src/morphodict/preference/test_preference.py
+++ src/morphodict/preference/test_preference.py
@@ -1,20 +1,23 @@
 import pytest
 
-from morphodict.preference import Preference, all_preferences, \
-    PreferenceConfigurationError
+from morphodict.preference import (
+    Preference,
+    PreferenceConfigurationError,
+    all_preferences,
+)
 
 
 def test_create_preference_with_incorrect_default():
+    """
+    Ensure that a preference MUST have a valid default.
+    """
     num_prefs_before = len(all_preferences())
 
     with pytest.raises(PreferenceConfigurationError):
+
         class PreferenceWithBadDefault(Preference):
-            choices = {
-                "coffee": "Coffee",
-                "tea": "Tea"
-            }
+            choices = {"coffee": "Coffee", "tea": "Tea"}
             default = "water"
 
     num_prefs_after = len(all_preferences())
     assert num_prefs_before == num_prefs_after
-
