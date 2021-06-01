8d5b8fcef173d2a4d2a7acea85b59a6320d8d858
Eddie Antonio Santos <eddieantonio@hey.com>
2021-05-31 19:24:57 -0600

    feat: implement preference option with context processor

--- src/morphodict/site/settings.py
+++ src/morphodict/site/settings.py
@@ -104,6 +104,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "CreeDictionary.CreeDictionary.context_processors.display_options",
+                "morphodict.preference.context_processors.preferences",
             ]
         },
     }
