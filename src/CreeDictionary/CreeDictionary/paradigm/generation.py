3adca5bd5f56dce3e9984518ba3420a205022398
Eddie Antonio Santos <eddieantonio@hey.com>
2021-06-04 15:53:01 -0600

    fix: use strict_generator in CreeDictionary

--- src/CreeDictionary/CreeDictionary/paradigm/generation.py
+++ src/CreeDictionary/CreeDictionary/paradigm/generation.py
@@ -6,10 +6,10 @@ from functools import cache
 
 from CreeDictionary.API.models import Wordform
 from CreeDictionary.CreeDictionary.paradigm.filler import Layout, ParadigmFiller
-from CreeDictionary.shared import expensive
 from CreeDictionary.utils import shared_res_dir
 from CreeDictionary.utils.enums import ParadigmSize
 from CreeDictionary.utils.fst_analysis_parser import extract_word_class
+from morphodict.analysis import strict_generator
 from morphodict.paradigm.manager import ParadigmManager
 
 
@@ -43,4 +43,4 @@ def default_paradigm_manager() -> ParadigmManager:
     Returns the ParadigmManager instance that loads layouts and FST from the res
     (resource) directory.
     """
-    return ParadigmManager(shared_res_dir / "layouts", expensive.strict_generator)
+    return ParadigmManager(shared_res_dir / "layouts", strict_generator())
