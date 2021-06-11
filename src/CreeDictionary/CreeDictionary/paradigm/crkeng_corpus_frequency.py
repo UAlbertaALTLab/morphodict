390630cbba99b73c63c19ad3c392df56b8d2b2f4
Eddie Antonio Santos <eddieantonio@hey.com>
2021-06-10 18:45:17 -0600

    refactor: Extract Function: import_tuples

--- src/CreeDictionary/CreeDictionary/paradigm/crkeng_corpus_frequency.py
+++ src/CreeDictionary/CreeDictionary/paradigm/crkeng_corpus_frequency.py
@@ -11,12 +11,17 @@ logger = logging.getLogger(__name__)
 def import_frequency() -> dict[ConcatAnalysis, int]:
     """
     Corpus frequency is stored in a bespoke file format. This parses that file and
-    returns a dictionary.
+    returns a dictionary mapping the ANALYSIS to the amount of times it has appeard
+    in the corpus.
     """
+    return {analysis: frequency for _, analysis, frequency in import_tuples()}
+
+
+def import_tuples() -> list[tuple[str, ConcatAnalysis, int]]:
     # TODO: store this in the database, rather than as a source file
     # TODO: make a management command that updates wordform frequencies
 
-    res: dict[ConcatAnalysis, int] = {}
+    result = []
     lines = CORPUS_FREQUENCY_FILE.read_text(encoding="UTF-8").splitlines()
     for line in lines:
         line = line.strip()
@@ -25,12 +30,15 @@ def import_frequency() -> dict[ConcatAnalysis, int]:
             continue
 
         try:
-            freq, _, *analyses = line.split()
+            freq, wordform, *analyses = line.split()
         except ValueError:
             # not enough value to unpack, which means the line has less than 3 values
             logger.warning(f'line "{line}" is broken in {CORPUS_FREQUENCY_FILE}')
-        else:
-            for analysis in analyses:
-                res[ConcatAnalysis(analysis)] = int(freq)
+            continue
 
-    return res
+        for analysis in analyses:
+            result.append(
+                (wordform, ConcatAnalysis(analysis), int(freq))
+            )
+
+    return result
