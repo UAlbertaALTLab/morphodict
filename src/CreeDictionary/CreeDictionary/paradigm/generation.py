ffafe31d49460fe39c970c840340e6ffc5ab1d0b
Eddie Antonio Santos <eddieantonio@hey.com>
2021-06-16 15:53:45 -0600

    feat: add MORPHODICT_PARADIGM_SIZE_ORDER for crkeng

--- src/CreeDictionary/CreeDictionary/paradigm/generation.py
+++ src/CreeDictionary/CreeDictionary/paradigm/generation.py
@@ -4,10 +4,13 @@ Handles paradigm generation.
 
 from functools import cache
 
+from django.conf import settings
+
 import morphodict.analysis
 from CreeDictionary.API.models import Wordform
 from CreeDictionary.CreeDictionary.paradigm.filler import Layout, ParadigmFiller
 from CreeDictionary.CreeDictionary.paradigm.manager import ParadigmManager
+from CreeDictionary.CreeDictionary.paradigm.sort_utils import identity, position_in_list
 from CreeDictionary.utils import shared_res_dir
 from CreeDictionary.utils.enums import ParadigmSize
 from CreeDictionary.utils.fst_analysis_parser import extract_word_class
@@ -42,7 +45,18 @@ def default_paradigm_manager() -> ParadigmManager:
     """
     Returns the ParadigmManager instance that loads layouts and FST from the res
     (resource) directory for the crk/eng language pair (itwêwina).
+
+    Affected by:
+      - MORPHODICT_PARADIGM_SIZE_ORDER
     """
+
+    if hasattr(settings, "MORPHODICT_PARADIGM_SIZE_ORDER"):
+        sorter = position_in_list(settings.MORPHODICT_PARADIGM_SIZE_ORDER)
+    else:
+        sorter = identity
+
     return ParadigmManager(
-        shared_res_dir / "layouts", morphodict.analysis.strict_generator()
+        shared_res_dir / "layouts",
+        morphodict.analysis.strict_generator(),
+        sort_sizes_by=sorter,
     )
