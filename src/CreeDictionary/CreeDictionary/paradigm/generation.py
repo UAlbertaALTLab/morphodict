2a1b827b47697fd0b41797fc51d6832ecae4a349
Eddie Antonio Santos <eddieantonio@hey.com>
2021-06-17 11:07:50 -0600

    remove: legacy generate_paradigm()

--- src/CreeDictionary/CreeDictionary/paradigm/generation.py
+++ src/CreeDictionary/CreeDictionary/paradigm/generation.py
@@ -5,36 +5,8 @@ Handles paradigm generation.
 from functools import cache
 
 import morphodict.analysis
-from CreeDictionary.API.models import Wordform
-from CreeDictionary.CreeDictionary.paradigm.filler import Layout, ParadigmFiller
 from CreeDictionary.CreeDictionary.paradigm.manager import ParadigmManager
 from CreeDictionary.utils import shared_res_dir
-from CreeDictionary.utils.enums import ParadigmSize
-from CreeDictionary.utils.fst_analysis_parser import extract_word_class
-
-
-def generate_paradigm(lemma: Wordform, size: ParadigmSize) -> list[Layout]:
-    """
-    :param lemma: the lemma of the desired paradigm
-    :param size: the level of detail for the paradigm.
-    :return: A list of filled paradigm tables.
-    """
-    # TODO: is there a better way to determine if this lemma inflects?
-    word_class = extract_word_class(lemma.analysis)
-
-    if word_class is None:
-        # Cannot determine how the the lemma inflects; no paradigm :/
-        return []
-
-    return paradigm_filler().fill_paradigm(lemma.text, word_class, size)
-
-
-@cache
-def paradigm_filler() -> ParadigmFiller:
-    """
-    Returns a cached instance of the default paradigm filler.
-    """
-    return ParadigmFiller.default_filler()
 
 
 @cache
