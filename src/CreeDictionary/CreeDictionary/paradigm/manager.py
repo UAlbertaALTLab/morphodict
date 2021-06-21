3982f32f312750643426367d493249522c81d061
Eddie Antonio Santos <eddieantonio@hey.com>
2021-06-21 10:23:35 -0600

    change: all_analyses may raise ParadigmDoesNotExistError

--- src/CreeDictionary/CreeDictionary/paradigm/manager.py
+++ src/CreeDictionary/CreeDictionary/paradigm/manager.py
@@ -65,12 +65,15 @@ class ParadigmManager:
         """
         Returns all analysis strings for a given paradigm and lemma in all layout sizes.
 
-        For example, in Cree, you want all analyses for mîcisow (VAI):
-            {"mîcisow+V+AI+Ind+Prs+1Sg", ...}
+        For example, in Plains Cree, you want all analyses for mîcisow (VAI):
+
+            {"mîcisow+V+AI+Ind+Prs+1Sg", "mîcisow+V+AI+Ind+Prs+2Sg", ...}
+
+        :raises ParadigmDoesNotExistError: when the paradigm name cannot be found.
         """
 
         analyses: set[str] = set()
-        for layout in self._name_to_layout[paradigm_name].values():
+        for layout in self._layout_sizes_or_raise(paradigm_name).values():
             analyses.update(layout.generate_fst_analyses(lemma).values())
 
         return analyses
