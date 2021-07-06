8448f8f60f8187109427414a8de6a07924f4bb26
Eddie Antonio Santos <eddieantonio@hey.com>
2021-07-06 09:18:10 -0600

    test: going to setting page sets a cookie

--- cypress/integration/settings.spec.js
+++ cypress/integration/settings.spec.js
@@ -6,7 +6,31 @@ context('The settings page', () => {
       .get('[data-cy=settings-link')
       .click()
 
-    cy.location()
+    cy.url()
       .should('match', /\bsettings\b/)
+  })
+
+  describe('setting a preference', () => {
+    const PREFERENCE_COOKIE = 'paradigmlabel'
+
+    it('should set the preference without a submit button', () => {
+      let checkedValue
+
+      cy.visit(Cypress.env('settings_url'))
+      cy.clearCookie(PREFERENCE_COOKIE)
+
+      cy.get(`input[name=${PREFERENCE_COOKIE}]`)
+        .last()
+        .check()
+        .then((inputJquery) => {
+          let input = inputJquery.get(0)
+          checkedValue = input.value
+        })
+
+      cy.getCookie(PREFERENCE_COOKIE)
+        .then((cookie) => {
+          expect(cookie.value).to.equal(checkedValue)
+        })
+    })
   })
 })
