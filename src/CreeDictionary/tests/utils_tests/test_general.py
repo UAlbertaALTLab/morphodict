2ad7c0d3705eb64c07214cb9c86581cca65cc402
Eddie Antonio Santos <eddieantonio@hey.com>
2021-07-08 22:14:09 -0600

    remove: tests for extract_word_class

--- src/CreeDictionary/tests/utils_tests/test_general.py
+++ src/CreeDictionary/tests/utils_tests/test_general.py
@@ -2,11 +2,9 @@
 separate test files need to be created for the tests in this file.
 todo: delete this file
 """
-import xml.etree.ElementTree as ET
 
 import pytest
-from CreeDictionary.utils import WordClass
-from CreeDictionary.utils.fst_analysis_parser import extract_lemma, extract_word_class
+from CreeDictionary.utils.fst_analysis_parser import extract_lemma
 
 
 @pytest.mark.parametrize(
@@ -26,22 +24,3 @@ from CreeDictionary.utils.fst_analysis_parser import extract_lemma, extract_word
 def test_hfstol_analysis_lemma_extraction(analysis, real_lemma):
     claimed = extract_lemma(analysis)
     assert claimed == real_lemma
-
-
-@pytest.mark.parametrize(
-    "analysis, category",
-    [
-        ("nôhkom+N+A+D+Px1Sg+Sg", WordClass("NAD")),
-        ("wâhkwa+N+A+Sg", WordClass("NA")),
-        ("maskwa +N+A+Der/Dim+N+A+Sg", WordClass("NA")),
-        ("PV/yikate+tihtipinêw+V+TA+Ind+Prs+3Sg+4Sg/PlO", WordClass("VTA")),
-        ("yîkatê-tihtipinam+V+TI+Ind+Prs+3Sg", WordClass("VTI")),
-        ("yîkatêpayin+V+II+Ind+Prs+3Sg", WordClass("VII")),
-        ("tânisi+Ipc", WordClass("IPC")),
-        ("mitêh+N+I+D+PxX+Sg", WordClass("NID")),
-        ("ôma+Pron+Def+Med+IN+Pl", WordClass("PRON")),
-    ],
-)
-def test_hfstol_analysis_category_extraction(analysis, category):
-    actual = extract_word_class(analysis)
-    assert actual == category
