369fbb66147ac90b5de5826871cb894374096015
Andrew Neitsch <andrew@neitsch.ca>
2021-08-04 10:20:03 -0600

    Remove creng paradigm assignment for now

--- munge/creng/run.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-/*
- * A launcher script for toimportjson.ts that sets up run-time transpilation.
- */
-
-const { runHelper } = require("../shared/run-helper");
-
-runHelper("toimportjson.ts");
