136897bfcb1044d1a23eadbe2630fb83d3da01a7
Jolene Poulin <jcpoulin@ualberta.ca>
2021-11-04 14:09:59 -0600

    checkpoint: deal with auto definitions

--- src/CreeDictionary/API/search/presentation.py
+++ src/CreeDictionary/API/search/presentation.py
@@ -257,7 +257,7 @@ def serialize_definitions(definitions, include_auto_definitions=False, dict_sour
         for source_id in serialized["source_ids"]:
             if source_id in dict_source:
                 ret.append(serialized)
-            elif include_auto_definitions and source_id.replace('🤖', '') in serialized["source_ids"]:
+            elif include_auto_definitions and '🤖' in source_id and source_id.replace('🤖', '') in serialized["source_ids"]:
                 ret.append(serialized)
     return ret
 
