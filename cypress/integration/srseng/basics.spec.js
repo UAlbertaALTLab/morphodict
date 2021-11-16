6b1fa15dd0250cd47cf9991c692b0b7f28886850
Jolene Poulin <jcpoulin@ualberta.ca>
2021-11-16 14:35:28 -0700

    fix: srs test for new db

--- cypress/integration/srseng/basics.spec.js
+++ cypress/integration/srseng/basics.spec.js
@@ -7,11 +7,11 @@ describe("The Tsúūt'ínà site", function () {
   });
 
   it("can search for a word", function () {
-    cy.visitSearch(`nas7in`, urls.srseng).searchResultsContain("násʔín");
+    cy.visitSearch(`ditsa`, urls.srseng).searchResultsContain("dītsá");
   });
 
   it("can display a paradigm", function () {
-    cy.visit(`${urls.srseng}/word/násʔín`);
-    cy.get(".paradigm-cell").contains("gīmīts'īnáyísʔìn");
+    cy.visit(`${urls.srseng}/word/dītsá`);
+    cy.get(".paradigm-cell").contains("dàts'īdītsá");
   });
 });
