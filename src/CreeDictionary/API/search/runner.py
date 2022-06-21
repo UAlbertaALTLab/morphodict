e0ea295221d5ad9ad83320d377eaf8999dd430ae
Jolene Poulin <jcpoulin@ualberta.ca>
2022-06-21 17:53:50 -0600

    fix: actually only run espt on multi-word queries

--- src/CreeDictionary/API/search/runner.py
+++ src/CreeDictionary/API/search/runner.py
@@ -39,7 +39,7 @@ def search(
         query=query, include_auto_definitions=include_auto_definitions
     )
 
-    if (search_run.query.espt or inflect_english_phrases) and (len(search_run.query.raw_query_string.split(" ")) > 1):
+    if (search_run.query.espt or inflect_english_phrases) and (len(search_run.query.query_terms) > 1):
         espt_search = EsptSearch(search_run)
         espt_search.analyze_query()
 
@@ -74,7 +74,7 @@ def search(
         ):
             do_cvd_search(search_run)
 
-    if (search_run.query.espt or inflect_english_phrases)  and (len(search_run.query.raw_query_string.split(" ")) > 1):
+    if (search_run.query.espt or inflect_english_phrases)  and (len(search_run.query.query_terms) > 1):
         espt_search.inflect_search_results()
 
     find_pos_matches(search_run)
