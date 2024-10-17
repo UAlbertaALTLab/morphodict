3a1ad6325a0c32817a5bd8a5c6e6933930368d7e
Felipe Bañados Schwerter <banadoss@ualberta.ca>
2024-10-17 20:01:57 +0000

    Updated use of secure middleware headers

--- src/morphodict/site/securemiddleware.py
+++ src/morphodict/site/securemiddleware.py
@@ -4,18 +4,18 @@ Use secure.py to set OWASP approved headers.
 Test security on by going to this site: https://securityheaders.com/
 
 See: https://owasp.org/www-project-secure-headers/
-Based on: https://secure.readthedocs.io/en/latest/frameworks.html#django
+Based on: https://github.com/TypeError/secure/blob/main/docs/frameworks.md#django
 """
 
-import secure
+from secure import Secure
 
-secure_headers = secure.Secure()
+secure_headers = Secure.with_default_headers()
 
 
 def set_secure_headers(get_response):
     def middleware(request):
         response = get_response(request)
-        secure_headers.framework.django(response)
+        secure_headers.set_headers(response)
         return response
 
     return middleware
