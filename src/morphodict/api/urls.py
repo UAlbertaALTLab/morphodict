b2faa9e483720d8c2dc7b6903ac78aedb9532592
Felipe Bañados Schwerter <banadoss@ualberta.ca>
2024-10-18 18:02:48 +0000

    Added URLS

---
+++ src/morphodict/api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+import morphodict.api.views as api_views
+
+################################ Click in text #################################
+# cree word translation for click-in-text
+urlpatterns = [
+        path(
+        "click-in-text/",
+        api_views.click_in_text,
+        name="cree-dictionary-word-click-in-text-api",
+    ),
+    path(
+        "click-in-text-embedded-test/",
+        api_views.click_in_text_embedded_test,
+        name="cree-dictionary-click-in-text-embedded-test",
+    ),
+]
