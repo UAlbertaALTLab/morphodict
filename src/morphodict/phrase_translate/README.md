c98bf0aad640a5dc72c0c6d10b1510fc00f6de4e
Felipe Bañados Schwerter <banadoss@ualberta.ca>
2024-11-05 17:45:41 +0000

    Refactored out some espt_crk dependencies

--- src/morphodict/phrase_translate/README.md
+++ src/morphodict/phrase_translate/README.md
@@ -11,7 +11,7 @@ The process to update the auto-translated phrases is as follows:
  3. Run the unit tests: `pipenv run pytest src`
 
  4. The english phrase FSTs are used:
-    - By by the `importjsondict` django command, which populates the auto definitions
+    - By the `importjsondict` django command, which populates the auto definitions
       for every inflected wordform when passed the `--translate-wordforms` flag (which is the default).  **There is no separate command to populate translated wordforms since 2022**.
     - During search, when building a `PhraseAnalyzedQuery` or an `AnalyzedQuery`
     - In the fst_tool admin interface
--- src/morphodict/search/espt_crk.py
+++ src/morphodict/search/espt_crk.py
@@ -3,9 +3,26 @@ Cree-specific data for English Inflected Phrase search
 """
 
 from morphodict.analysis.tag_map import TagMap
+from django.conf import settings
+import json
+import re
+
+
+def read_phrase_translate_json(filename, default=[]):
+    # This helper function parses the contents of our phrase_translate elements *after* removing comments.
+    # Note that, in general, this might interact with the JSON file contents, but we are not using # for
+    # anything in our json files yet, so we can use them.
+    try:
+        with open(settings.BASE_DIR / "resources" / "phrase_translate" / filename, "r") as f:
+            contents = f.read()
+            ans = json.loads(re.sub("(^|\s)+#.*\n", "", contents))
+    except FileNotFoundError:
+        return default
+    return ans
+
 
 # tags needed for FST generator
-crk_noun_tags = ["+N", "+A", "+I", "+D"]
+crk_noun_tags = read_phrase_translate_json("noun_tags.json")
 
 
 # TagMaps can’t automatically pass through unknown tags because they wouldn’t
@@ -14,127 +31,36 @@ crk_noun_tags = ["+N", "+A", "+I", "+D"]
 # +V+TA and before +1Sg, so it needs precedence values for all of those tags.
 #
 # So we list everything.
-noun_passthrough_tags = {
-    0: [
-        # word class
-        "+N",
-        "+A",
-        "+I",
-        "+D",
-    ],
-    2: [
-        # N: Possessives
-        "+Px1Sg",
-        "+Px2Sg",
-        "+Px3Sg",
-        "+Px1Pl",
-        "+Px2Pl",
-        # "+Px12Pl", # Needs to be recoded: 21 -> 12
-        "+Px3Pl",
-        "+Px4Sg/Pl",
-        "+PxX",
-        "+PxXPl",
-    ],
-    3: [
-        # N: number
-        "+Sg",
-        "+Pl",
-        "+Obv",
-        "+Loc",
-        "+Distr",
-    ],
-}
+noun_passthrough_tags = read_phrase_translate_json("noun_passthrough_tags.json",{})
 
-verb_passthrough_tags = {
-    0: [
-        # word class
-        "+V",
-        "+TA",
-        "+AI",
-        "+II",
-        "+TI",
-    ],
-    2: [
-        # V: Person - subject
-        "+1Sg",
-        "+2Sg",
-        "+3Sg",
-        "+1Pl",
-        # "+12Pl", # Needs to be recoded: 21 -> 12
-        "+2Pl",
-        "+3Pl",
-        "+4Sg/Pl",
-        "+5Sg/Pl",
-        "+X",
-        "+XPl",
-    ],
-    3: [
-        # V: Person - object
-        "+1SgO",
-        "+2SgO",
-        "+3SgO",
-        "+1PlO",
-        # "+21PlO", # Needs to be recoded: 21 -> 12
-        "+2PlO",
-        "+3PlO",
-        "+4Pl",
-        "+4Sg",
-        "+4Sg/PlO",
-        "+5Sg/PlO",
-        "+XO",
-        "+XPlO",
-    ],
-}
+verb_passthrough_tags = read_phrase_translate_json("verb_passthrough_tags.json",{})
 
 
 def passthrough_tags_to_tuples(passthrough_tags):
     return (
-        (tag, tag, precedence)
+        (tag, tag, int(precedence))
         for precedence, tag_list in passthrough_tags.items()
         for tag in tag_list
     )
 
+def lists_to_tuple(l):
+    def element_process(x):
+        if isinstance(x,list):
+            return tuple(x)
+        if x == "TagMap.DEFAULT":
+            return TagMap.DEFAULT
+        return x
+    return tuple(element_process(x) for x in l)
+
+def tagmap_json_to_tuples(json):
+    return [lists_to_tuple(t) for t in json]
 
 verb_tag_map = TagMap(
-    # Tense
-    ("+Prt", ("PV/ki+", "+Ind"), 1),  # Preterite aka simple past
-    ("+Cond", ("+Fut", "+Cond"), 1),  # Future conditional
-    ("+Imm", ("+Imp", "+Imm"), 1),  # Immediate imperative
-    ("+Del", ("+Imp", "+Del"), 1),  # Delayed imperative
-    ("+Fut", ("PV/wi+", "+Ind"), 1),  # Future
-    # TODO: also handle ("+Fut", "PV/wi+", 1)  # Also accept PV/wi without independent as future?
-    # Note that these crk features as disjoint, but both are needed for the eng feature
-    ("+Def", ("PV/ka+", "+Ind"), 1),
-    ("+Inf", ("PV/ka+", "+Cnj"), 1),
-    (TagMap.DEFAULT, "+Ind", 1),
-    # Person - see https://github.com/UAlbertaALTLab/morphodict/issues/891
-    ("+0Sg", "+3Sg", 2),
-    (
-        "+21Pl",
-        "+12Pl",
-        2,
-    ),  # see https://github.com/UAlbertaALTLab/morphodict/issues/1005
-    ("+XPl", "+X", 2),
-    # Person - object
-    ("+0SgO", (), 3),
-    (
-        "+21PlO",
-        "+12PlO",
-        3,
-    ),
-    ("+XPlO", "+XO", 3),
-    (
-        "+PV/pimi",
-        ("PV/pimi+"),
-        3,
-    ),  # see https://github.com/UAlbertaALTLab/morphodict/issues/1005
-    # TODO: also handle "+Inf": ("PV/ta+", "+Cnj")  # future definite?
+    *tagmap_json_to_tuples(read_phrase_translate_json("verb_tag_map.json")),
     *passthrough_tags_to_tuples(verb_passthrough_tags)
 )
 
 noun_tag_map = TagMap(
-    ("+Dim", "+Der/Dim", 2),
-    ("+Px21Pl", "+Px12Pl", 2),
-    ("+PxXPl", "+PxX", 2),
+    *tagmap_json_to_tuples(read_phrase_translate_json("noun_tag_map.json")),
     *passthrough_tags_to_tuples(noun_passthrough_tags)
 )
