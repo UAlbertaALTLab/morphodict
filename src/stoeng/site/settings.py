3e2f9f62422a8db83874b154cedbe11a9df361fc
Felipe Bañados Schwerter <banadoss@ualberta.ca>
2024-11-19 01:21:39 +0000

    Rest of the required stoeng files

--- src/stoeng/site/settings.py
+++ src/stoeng/site/settings.py
@@ -19,9 +19,10 @@ base_dir_setup.set_base_dir(BASE_DIR)
 from morphodict.site.settings import *
 
 # Where this application should be deployed:
-PRODUCTION_HOST = "stoeng.altlab.dev"
+PRODUCTION_HOST = "nakon-ie.altlab.dev"
 
 ALLOWED_HOSTS.append(PRODUCTION_HOST)
+CSRF_TRUSTED_ORIGINS.append("https://" + PRODUCTION_HOST)
 
 DEFAULT_RUNSERVER_PORT = 8018
 
@@ -34,6 +35,16 @@ MORPHODICT_ISO_639_1_CODE = "sto"
 
 MORPHODICT_SOURCE_LANGUAGE = "sto"
 MORPHODICT_TARGET_LANGUAGE = "eng"
+
+MORPHODICT_DICTIONARY_NAME = "nakón-i’e"
+MORPHODICT_LANGUAGE_ENDONYM = "Nakoda"
+MORPHODICT_SOURCE_LANGUAGE_NAME = "Stoney Nakoda"
+MORPHODICT_SOURCE_LANGUAGE_SHORT_NAME = "Nakoda"    
+
+RELAXED_ANALYZER_FST_FILENAME = "analyser-gt-dict-norm.hfstol"
+STRICT_ANALYZER_FST_FILENAME = RELAXED_ANALYZER_FST_FILENAME
+STRICT_GENERATOR_FST_FILENAME = "generator-gt-dict-norm.hfstol"
+
 
 MORPHODICT_ORTHOGRAPHY = {
     "default": "Latn",
