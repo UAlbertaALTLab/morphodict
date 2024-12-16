e8a151a83c093c5c7876f6989f8cfe10429cb183
Felipe Bañados Schwerter <banadoss@ualberta.ca>
2024-12-16 18:57:21 +0000

    Draft of accordion

--- frontend/index.js
+++ frontend/index.js
@@ -29,6 +29,9 @@ import * as settings from "./js/settings-page.js";
 import * as toast from "./js/toast.js";
 import { loadParadigmAudio } from "./js/paradigm-recording";
 
+import '../node_modules/bootstrap/js/dist/collapse.js'
+//import './css/bootstrap.scss';
+//import '../node_modules/bootstrap/dist/css/bootstrap.css'
 ///////////////////////////////// Constants //////////////////////////////////
 
 const NO_BREAK_SPACE = "\u00A0";
