065e59ae6942290d8c8221ee362324a3e4ea20c1
Felipe Bañados Schwerter <banadoss@ualberta.ca>
2025-01-20 23:28:23 +0000

    JS reformat

--- cypress/e2e/srseng/basics.spec.js
+++ cypress/e2e/srseng/basics.spec.js
@@ -2,8 +2,8 @@ import urls from "../../support/urls";
 
 describe("The Tsúūt'ínà site", function () {
   this.beforeEach(() => {
-    const cookies = Cypress.env('cookies') || []
-    cookies.forEach(c => cy.setCookie(c.name,c.value))
+    const cookies = Cypress.env("cookies") || [];
+    cookies.forEach((c) => cy.setCookie(c.name, c.value));
   });
 
   it("works", function () {
@@ -12,25 +12,25 @@ describe("The Tsúūt'ínà site", function () {
   });
 
   it("can login before anything", function () {
-    cy.user_login(`${urls.srseng}/accounts/login/?next=/`)
-    cy.getCookies().then(cookies => {
-      var values=""
-      cookies.forEach(c => values+=`{${c.name},${c.value}}`)
+    cy.user_login(`${urls.srseng}/accounts/login/?next=/`);
+    cy.getCookies().then((cookies) => {
+      var values = "";
+      cookies.forEach((c) => (values += `{${c.name},${c.value}}`));
       Cypress.log({
         name: "cookies",
         message: `${values}`,
-      })
-      Cypress.env('cookies', cookies)
-  })
-  cy.visit(`${urls.srseng}`);
+      });
+      Cypress.env("cookies", cookies);
+    });
+    cy.visit(`${urls.srseng}`);
   });
 
   it("can search for a word", function () {
-        cy.visitSearch(`yidiskid`, urls.srseng).searchResultsContain("yídiskid");
+    cy.visitSearch(`yidiskid`, urls.srseng).searchResultsContain("yídiskid");
   });
 
   it("can display a paradigm", function () {
     cy.visit(`${urls.srseng}/word/yídiskid`);
     cy.get(".paradigm-cell").contains("mídiskid");
-  }); 
+  });
 });
