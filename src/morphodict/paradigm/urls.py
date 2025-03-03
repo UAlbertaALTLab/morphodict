40b63a48ff7fd5d2d5daecfa4aa0233ed88d0dc2
Felipe Bañados Schwerter <banadoss@ualberta.ca>
2025-03-03 18:27:03 +0000

    Added paradigm to API subspace

--- src/morphodict/paradigm/urls.py
+++ src/morphodict/paradigm/urls.py
@@ -6,11 +6,11 @@ from morphodict.paradigm import views
 urlpatterns = [
     # internal use to render paradigm and only the paradigm
     path(
-        "_paradigm_details/",
+        "api/paradigm_details/",
         views.paradigm_internal,
         name="morphodict-paradigm-detail",
     ),
     path(
-        "_paradigm_layout/", views.paradigm_for_lemma, name="morphodict-paradigm-layout"
+        "api/paradigm_layout/", views.paradigm_for_lemma, name="morphodict-paradigm-layout"
     ),
 ]
