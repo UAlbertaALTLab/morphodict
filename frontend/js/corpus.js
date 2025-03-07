ab50e84c3784921f8eaba2d51479cf8459f47bee
Felipe Bañados Schwerter <banadoss@ualberta.ca>
2025-03-07 21:16:46 +0000

    Added missing file

---
+++ frontend/js/corpus.js
@@ -0,0 +1,30 @@
+const COUNT_API = "https://korp-backend.altlab.dev/count";
+const CORPUS_URL = "https://korp.altlab.dev/"
+const CORPORA = getCorporaFromLocation();
+function getCorporaFromLocation() {
+    const location = window.location.toString();
+    if (location.includes(`itwewina`) || location.includes(`crk`)) {
+        return "MASKWACIS-SENTENCES,WOLFART_AHENAKEW"
+    }
+    return ""
+}
+
+function createCorpusSearchURL(wordform){
+    return `${CORPUS_URL}#?corpus=${CORPORA.toLowerCase()}&search=word|${wordform}&cqp=[]`
+}
+
+export async function fetchCorpusURL(wordform) {
+    if (!CORPORA) {
+        return undefined
+    }
+    let result = await fetch(
+        `${COUNT_API}?corpus=${CORPORA}&cqp=[word="${wordform}"]`)
+            .then((count_request) => {
+            return count_request.json()
+            }).then((count_json) => {
+                if (count_json["count"]){
+                    return createCorpusSearchURL(wordform)
+                }
+            })
+    return result
+}
