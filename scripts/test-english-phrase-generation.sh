d3256c76bcb141d8a34680db7d9497b8c599b581
antti.arppe@iki.fi <antti.arppe@iki.fi>
2025-03-22 02:31:44 -0600

    Adjusted script to deal with ta-conjuncts, multi-sense definitions, and explicit TI object tagging.

--- scripts/test-english-phrase-generation.sh
+++ scripts/test-english-phrase-generation.sh
@@ -39,12 +39,19 @@ gawk -v PARADIGM=$1 -v DICTIONARY=$2 -v CORPUS=$3 -v ENGGENFST=$4 'BEGIN { pdgm=
   for(i=1; i<=nr; i++)
      {
        nf=split(rec[i], f, "\n");
-       ns=0;
+       six=0;
        for(j=1; j<=nf; j++)
           {
             if(match(f[j], "\"head\": \"([^\"]+)\"", ff)!=0) { entry=ff[1]; six=0; }
             if(match(f[j], "\"paradigm\": \"([^\"]+)\"", ff)!=0) pos=ff[1];
-            if(match(f[j], "\"definition\": \"([^\"]+)\"", ff)!=0) sense[entry "_" pos][ff[1]]=++six;
+#            if(match(f[j], "\"definition\": \"([^\"]+)\"", ff)!=0) sense[entry "_" pos][ff[1]]=++six;
+            if(match(f[j], "\"definition\": \"([^\"]+)\"", ff)!=0)
+              {
+                def=ff[1];
+                ns=split(def, fff, "[ ]*[;][ ]*");
+                for(k=1; k<=ns; k++)
+                   sense[entry "_" pos][fff[k]]=++six;
+              }
 	  }
      }
   # for(s in sense) for(d in sense[s]) print s, d;
@@ -73,11 +80,13 @@ gawk -v PARADIGM=$1 -v DICTIONARY=$2 -v CORPUS=$3 -v ENGGENFST=$4 'BEGIN { pdgm=
                   if(index(crk_anl, "PV/ka+")!=0 && index(crk_anl, "+Cnj")==0) tense="Def+";
                   if(index(crk_anl, "PV/wi+")!=0) tense="Fut+";
                   if(index(crk_anl, "PV/ka+")!=0 && index(crk_anl, "+Cnj")!=0) tense="Inf+";
+                  if(index(crk_anl, "PV/ta+")!=0 && index(crk_anl, "+Cnj")!=0) tense="Inf+";
                   if(index(crk_anl, "+Cond")!=0) tense="Cond+";
                   if(index(crk_anl, "+Imp+Imm")!=0) tense="Imm+";
                   if(index(crk_anl, "+Imp+Del")!=0) tense="Del+";
                   if(match(crk_anl, "\\+([12345X][^\\+O]*)", f)!=0) subj=f[1] "+";
                   if(match(crk_anl, "\\+([12345X][^\\+O]*O)", f)!=0) obj=f[1] "+"
+                  if(POS=="VTI") obj="0SgO+";
                   sub("12", "21", subj); sub("12", "21", obj);
                   sub("X","XPl", subj); sub("XO","XPlO", obj);
                   if(tense=="") tense="Prs+";
@@ -94,14 +103,18 @@ gawk -v PARADIGM=$1 -v DICTIONARY=$2 -v CORPUS=$3 -v ENGGENFST=$4 'BEGIN { pdgm=
                   # if(lemma "_" POS in sense)
                     for(def in sense[lemma "_" POS])
                      {
-                       gsub("[ ]\\[[^\\]]+\\]", "", def);
-                       gsub("[ ]\\(e\\.g\\.[^\\)]+\\)", "", def);
-                       gsub("[ ]\\(i\\.e\\.[^\\)]+\\)", "", def);
+                       def0=def;
+                       gsub("[ ]\\[[^\\]]+\\]", "", def0);
+                       gsub("[ ]\\(e\\.g\\.[^\\)]+\\)", "", def0);
+                       gsub("[ ]\\(i\\.e\\.[^\\)]+\\)", "", def0);
+
+                       if(match(def0, " s\\.[ot]\\.$")==0)
+                         sub("\\.$", "", def0);
 
                        six=sense[lemma "_" POS][def];
 
-                       eng_phr=lookup("flookup -i -b", fst, eng_anl def);
-                       printf "%i / %i - %s (%i): %s -> %s\t%s\n", pos_ix, six, lemma, frq["VTA"][lemma], crk_anl, eng_anl, eng_phr;
+                       eng_phr=lookup("flookup -i -b", fst, eng_anl def0);
+                       printf "%i / %i - %s (%i): %s -> %s\t%s\n", pos_ix, six, lemma, frq[POS][lemma], crk_anl, eng_anl, eng_phr;
                      }
                 }
             }
