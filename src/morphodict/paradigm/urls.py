ef93b6e6181cdbd2abb3e870a28972f53dce6e66
Felipe Bañados Schwerter <banadoss@ualberta.ca>
2025-04-14 22:36:30 +0000

    Deploy v1 of APIs - Still needs documentation

--- src/morphodict/paradigm/urls.py
+++ src/morphodict/paradigm/urls.py
@@ -11,6 +11,13 @@ urlpatterns = [
         name="morphodict-paradigm-detail",
     ),
     path(
-        "_paradigm_layout/", views.paradigm_for_lemma, name="morphodict-paradigm-layout"
+        "api/paradigm_layout/html",
+        views.paradigm_for_lemma,
+        name="morphodict-paradigm-layout",
+    ),
+    path(
+        "api/paradigm_layout/json",
+        views.json_paradigm_for_lemma,
+        name="morphodict-json-paradigm-layout",
     ),
 ]
